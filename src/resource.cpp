b16925fa5ffa92398814961d1ca9506450aa60ae
cfillion <cfillion@users.noreply.github.com>
2021-05-04 03:32:30 -0400

    cleanup Resource::Timer
    (cherry picked from commit 765d51172b3a0266095e89bb2b8bfc98a189fc07)

--- src/resource.cpp
+++ src/resource.cpp
@@ -21,6 +21,7 @@
 
 #include <reaper_plugin_functions.h>
 #include <unordered_set>
+#include <WDL/wdltypes.h>
 
 constexpr size_t MAX_INSTANCES { 99 };
 
@@ -30,75 +31,76 @@ static unsigned int g_reentrant;
 static WNDPROC g_mainProc;
 #endif
 
-#ifndef GWLP_WNDPROC // SWELL
-#  define GWLP_WNDPROC GWL_WNDPROC
-#  define SetWindowLongPtr SetWindowLong
-#  define GetWindowLongPtr GetWindowLong
-#endif
-
 class Resource::Timer {
 public:
-  Timer()
-  {
-#ifndef __APPLE__
-    LONG_PTR newProc { reinterpret_cast<LONG_PTR>(&mainProcOverride) },
-             oldProc { SetWindowLongPtr(GetMainHwnd(), GWLP_WNDPROC, newProc) };
-    g_mainProc = reinterpret_cast<WNDPROC>(oldProc);
-#endif
-
-    plugin_register("timer", reinterpret_cast<void *>(&Timer::tick));
-  }
-
-  ~Timer()
-  {
-    plugin_register("-timer", reinterpret_cast<void *>(&Timer::tick));
-
-#ifndef __APPLE__
-    HWND mainWnd { GetMainHwnd() };
-    LONG_PTR expectedProc { reinterpret_cast<LONG_PTR>(&mainProcOverride) },
-             previousProc { reinterpret_cast<LONG_PTR>(g_mainProc) };
-    if(GetWindowLongPtr(mainWnd, GWLP_WNDPROC) == expectedProc)
-      SetWindowLongPtr(mainWnd, GWLP_WNDPROC, previousProc);
-#endif
-  }
+  Timer();
+  ~Timer();
 
 private:
-  static void tick()
-  {
-    // REAPER v6.19+ does not execute deferred script callbacks
-    // when the splash screen is open.
-    if(Splash_GetWnd() || g_reentrant > 1)
-      return;
+  static void tick();
+  static LRESULT CALLBACK mainProcOverride(HWND, unsigned int, WPARAM, LPARAM);
+};
 
-    auto it { g_rsx.begin() };
+Resource::Timer::Timer()
+{
+#ifndef __APPLE__
+  LONG_PTR newProc { reinterpret_cast<LONG_PTR>(&mainProcOverride) },
+           oldProc { SetWindowLongPtr(GetMainHwnd(), GWLP_WNDPROC, newProc) };
+  g_mainProc = reinterpret_cast<WNDPROC>(oldProc);
+#endif
 
-    while(it != g_rsx.end()) {
-      Resource *rs { *it++ };
-      if(!rs->heartbeat())
-        delete rs;
-    }
-  }
+  plugin_register("timer", reinterpret_cast<void *>(&Timer::tick));
+}
+
+Resource::Timer::~Timer()
+{
+  plugin_register("-timer", reinterpret_cast<void *>(&Timer::tick));
 
 #ifndef __APPLE__
+  HWND mainWnd { GetMainHwnd() };
+  LONG_PTR expectedProc { reinterpret_cast<LONG_PTR>(&mainProcOverride) },
+           previousProc { reinterpret_cast<LONG_PTR>(g_mainProc) };
+  if(GetWindowLongPtr(mainWnd, GWLP_WNDPROC) == expectedProc)
+    SetWindowLongPtr(mainWnd, GWLP_WNDPROC, previousProc);
+#endif
+}
+
+void Resource::Timer::tick()
+{
+  // REAPER v6.19+ does not execute deferred script callbacks
+  // when the splash screen is open.
+  if(Splash_GetWnd() || g_reentrant > 1)
+    return;
+
+  auto it { g_rsx.begin() };
+
+  while(it != g_rsx.end()) {
+    Resource *rs { *it++ };
+    if(!rs->heartbeat())
+      delete rs;
+  }
+}
+
+#ifndef __APPLE__
+LRESULT CALLBACK Resource::Timer::mainProcOverride(HWND hwnd,
+  unsigned int msg, WPARAM wParam, LPARAM lParam)
+{
   // Timers are reentrant on Windows and Linux, but deferred ReaScripts aren't.
   // The workaround below suspends ReaImGui when a script opens a modal dialog,
-  // in order to not flag the context as being unused and destroying it.
-  static LRESULT CALLBACK mainProcOverride(HWND hwnd,
-    unsigned int msg, WPARAM wParam, LPARAM lParam)
-  {
-    // 0x29a is REAPER's "misc timer". It's responsible for triggering both
-    // deferred ReaScripts and extension timer callbacks (among other things).
-    if(msg == WM_TIMER && wParam == 0x29a) {
-      g_reentrant += 1;
-      const LRESULT ret { CallWindowProc(g_mainProc, hwnd, msg, wParam, lParam) };
-      g_reentrant -= 1;
-      return ret;
-    }
-
-    return CallWindowProc(g_mainProc, hwnd, msg, wParam, lParam);
+  // in order to not flag active contexts as being unused and destroying them.
+  //
+  // 0x29a is REAPER's "misc timer". It's responsible for triggering both
+  // deferred ReaScripts and extension timer callbacks (among other things).
+  if(msg == WM_TIMER && wParam == 0x29a) {
+    g_reentrant += 1;
+    const LRESULT ret { CallWindowProc(g_mainProc, hwnd, msg, wParam, lParam) };
+    g_reentrant -= 1;
+    return ret;
   }
+
+  return CallWindowProc(g_mainProc, hwnd, msg, wParam, lParam);
+}
 #endif
-};
 
 Resource::Resource()
 {
