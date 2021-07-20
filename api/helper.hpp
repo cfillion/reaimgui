cfillion/reaimgui

commit a4e7d964cdf5b4da66012efdc51e05c296085753

document p_* arguments as both read and write [p=2459747]

api/menu.cpp
@@ -50,7 +50,7 @@ R"(Only call EndMenu() if ImGui_BeginMenu returns true!)",
 
 DEFINE_API(__LINE__, bool, MenuItem, (ImGui_Context*,ctx)
 (const char*,label)(const char*,API_RO(shortcut))
-(bool*,API_W(p_selected))(bool*,API_RO(enabled)),
+(bool*,API_RW(p_selected))(bool*,API_RO(enabled)),
 R"(Return true when activated. Shortcuts are displayed for convenience but not processed by ImGui at the moment. Toggle state is written to 'selected' when provided.
 
 Default values: enabled = true)",
@@ -58,6 +58,6 @@ Default values: enabled = true)",
   FRAME_GUARD;
   nullIfEmpty(API_RO(shortcut));
 
-  return ImGui::MenuItem(label, API_RO(shortcut), API_W(p_selected),
+  return ImGui::MenuItem(label, API_RO(shortcut), API_RW(p_selected),
     valueOr(API_RO(enabled), true));
 });

api/popup.cpp
@@ -39,13 +39,13 @@ Default values: flags = ImGui_WindowFlags_None)",
 });
 
 DEFINE_API(__LINE__, bool, BeginPopupModal, (ImGui_Context*,ctx)
-(const char*,name)(bool*,API_W(p_open))(int*,API_RO(flags)),
+(const char*,name)(bool*,API_RW(p_open))(int*,API_RO(flags)),
 R"(Block every interactions behind the window, cannot be closed by user, add a dimming background, has a title bar. Return true if the modal is open, and you can start outputting to it. See ImGui_BeginPopup.
 
 Default values: flags = ImGui_WindowFlags_None)",
 {
   FRAME_GUARD;
-  return ImGui::BeginPopupModal(name, openPtrBehavior(API_W(p_open)),
+  return ImGui::BeginPopupModal(name, openPtrBehavior(API_RW(p_open)),
     valueOr(API_RO(flags), ImGuiWindowFlags_None));
 });
 

api/select.cpp
@@ -126,7 +126,7 @@ DEFINE_ENUM(ImGui, ComboFlags_NoArrowButton,  "Display on the preview box withou
 DEFINE_ENUM(ImGui, ComboFlags_NoPreview,      "Display only a square arrow button.");
 
 DEFINE_API(__LINE__, bool, Selectable, (ImGui_Context*,ctx)
-(const char*,label)(bool*,API_W(p_selected))
+(const char*,label)(bool*,API_RW(p_selected))
 (int*,API_RO(flags))(double*,API_RO(size_w))(double*,API_RO(size_h)),
 R"(A selectable highlights when hovered, and can display another color when selected.
 Neighbors selectable extend their highlight bounds in order to leave no gap between them. This is so a series of selected Selectable appear contiguous.
@@ -135,7 +135,7 @@ Default values: flags = ImGui_SelectableFlags_None, size_w = 0.0, size_h = 0.0)"
 {
   FRAME_GUARD;
   bool selectedOmitted {};
-  bool *selected { API_W(p_selected) ? API_W(p_selected) : &selectedOmitted };
+  bool *selected { API_RW(p_selected) ? API_RW(p_selected) : &selectedOmitted };
   const ImGuiSelectableFlags flags
     { valueOr(API_RO(flags), ImGuiSelectableFlags_None) };
   const ImVec2 size { valueOr(API_RO(size_w), 0.f),

api/tabbar.cpp
@@ -35,13 +35,13 @@ DEFINE_API(__LINE__, void, EndTabBar, (ImGui_Context*,ctx),
 });
 
 DEFINE_API(__LINE__, bool, BeginTabItem, (ImGui_Context*,ctx)
-(const char*,label)(bool*,API_W(p_open))(int*,API_RO(flags)),
+(const char*,label)(bool*,API_RW(p_open))(int*,API_RO(flags)),
 R"(Create a Tab. Returns true if the Tab is selected. Set 'p_open' to true to enable the close button.
 
 Default values: flags = ImGui_TabItemFlags_None)",
 {
   FRAME_GUARD;
-  return ImGui::BeginTabItem(label, openPtrBehavior(API_W(p_open)),
+  return ImGui::BeginTabItem(label, openPtrBehavior(API_RW(p_open)),
     valueOr(API_RO(flags), ImGuiTabItemFlags_None));
 });
 

api/treenode.cpp
@@ -61,7 +61,7 @@ DEFINE_API(__LINE__, double, GetTreeNodeToLabelSpacing, (ImGui_Context*,ctx),
 });
 
 DEFINE_API(__LINE__, bool, CollapsingHeader, (ImGui_Context*,ctx)
-(const char*,label)(bool*,API_W(p_visible))(int*,API_RO(flags)),
+(const char*,label)(bool*,API_RW(p_visible))(int*,API_RO(flags)),
 R"(Returns true when opened but do not indent nor push into the ID stack (because of the ImGui_TreeNodeFlags_NoTreePushOnOpen flag).
 
 This is basically the same as calling TreeNode(label, ImGui_TreeNodeFlags_CollapsingHeader). You can remove the _NoTreePushOnOpen flag if you want behavior closer to normal ImGui_TreeNode.
@@ -75,7 +75,7 @@ Default values: flags = ImGui_TreeNodeFlags_None)",
   // as NULL. This is because EEL doesn't have a NULL (0 = false), API_W never
   // receives a NULL, and API_RWO aren't listed in the output values list.
   const ImGuiTreeNodeFlags flags { valueOr(API_RO(flags), ImGuiTreeNodeFlags_None) };
-  return ImGui::CollapsingHeader(label, openPtrBehavior(API_W(p_visible)), flags);
+  return ImGui::CollapsingHeader(label, openPtrBehavior(API_RW(p_visible)), flags);
 });
 
 DEFINE_API(__LINE__, void, SetNextItemOpen, (ImGui_Context*,ctx)

api/window.cpp
@@ -20,7 +20,7 @@
 #include <imgui/imgui.h>
 
 DEFINE_API(__LINE__, bool, Begin, (ImGui_Context*,ctx)
-(const char*,name)(bool*,API_W(p_open))(int*,API_RO(flags)),
+(const char*,name)(bool*,API_RW(p_open))(int*,API_RO(flags)),
 R"(Push window to the stack and start appending to it. See ImGui_End.
 
 - Passing true to 'p_open' shows a window-closing widget in the upper-right corner of the window, which clicking will set the boolean to false when returned.
@@ -40,7 +40,7 @@ Default values: flags = ImGui_WindowFlags_None)",
              ImGuiWindowFlags_NoResize   ;
   }
 
-  const bool rv { ImGui::Begin(name, openPtrBehavior(API_W(p_open)), flags) };
+  const bool rv { ImGui::Begin(name, openPtrBehavior(API_RW(p_open)), flags) };
 
   if(!ctx->IO().ConfigViewportsNoDecoration)
     ImGui::PopStyleVar();
@@ -492,11 +492,11 @@ Default values: center_y_ratio = 0.5)",
 });
 
 DEFINE_API(__LINE__, void, ShowMetricsWindow, (ImGui_Context*,ctx)
-(bool*,API_W(p_open)),
+(bool*,API_RW(p_open)),
 R"(Create Metrics/Debugger window. Display Dear ImGui internals: windows, draw commands, various internal state, etc. Set p_open to true to enable the close button.)",
 {
   FRAME_GUARD;
-  ImGui::ShowMetricsWindow(openPtrBehavior(API_W(p_open)));
+  ImGui::ShowMetricsWindow(openPtrBehavior(API_RW(p_open)));
 });
 
 // ImGuiFocusedFlags

commit 2013b3e944351f1d0d9c1d060c47737b600c4e20

fix syntax error in the Python binding [#4]
Regression from a4e7d964cdf5b4da66012efdc51e05c296085753.

api/helper.hpp
@@ -66,6 +66,7 @@
 
 #define API_RO(var)       var##InOptional // read, optional/nullable (except string, use nullIfEmpty)
 #define API_RW(var)       var##InOut      // read/write
+#define API_RWO(var)      var##InOutOptional // documentation/python only
 #define API_W(var)        var##Out        // write
 #define API_W_SZ(var)     var##Out_sz     // write
 // Not using varInOutOptional because REAPER refuses to pass NULL to them

api/menu.cpp
@@ -50,7 +50,7 @@ R"(Only call EndMenu() if ImGui_BeginMenu returns true!)",
 
 DEFINE_API(bool, MenuItem, (ImGui_Context*,ctx)
 (const char*,label)(const char*,API_RO(shortcut))
-(bool*,API_RW(p_selected))(bool*,API_RO(enabled)),
+(bool*,API_RWO(p_selected))(bool*,API_RO(enabled)),
 R"(Return true when activated. Shortcuts are displayed for convenience but not processed by ImGui at the moment. Toggle state is written to 'selected' when provided.
 
 Default values: enabled = true)",
@@ -58,6 +58,6 @@ Default values: enabled = true)",
   FRAME_GUARD;
   nullIfEmpty(API_RO(shortcut));
 
-  return ImGui::MenuItem(label, API_RO(shortcut), API_RW(p_selected),
+  return ImGui::MenuItem(label, API_RO(shortcut), API_RWO(p_selected),
     valueOr(API_RO(enabled), true));
 });