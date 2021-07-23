cfillion/reaimgui

commit 0ba452e8ac469b493e7cbf0ce7f9860ba2b09282

genbinding: fix debug assert failure when generating html doc on Windows

tools/genbinding.cpp
@@ -500,7 +500,8 @@ static void formatHtmlText(std::ostream &stream, const std::string_view &text)
   for(size_t i {}; i < text.size(); ++i) {
     size_t linkStart { i };
     if(i == nextLink) {
-      do { ++i; } while(isalnum(text[i]) || text[i] == '_');
+      while(i < text.size() - 1 && (isalnum(text[i]) || text[i] == '_'))
+        ++i;
       const std::string_view linkTo { &text[linkStart], i - linkStart };
       if(std::binary_search(g_funcs.begin(), g_funcs.end(), linkTo, FunctionComp{}))
         stream << "<a href=\"#" << linkTo << "\">" << linkTo << "</a>";
@@ -518,8 +519,6 @@ static void formatHtmlText(std::ostream &stream, const std::string_view &text)
       break;
     case '&':
       stream << "&amp;";
-      break;
-    case '\0': // if the string ended with a link
       break;
     default:
       stream << text[i];

commit 515aa7559a77205088660f573cd98044ca6a8c97

genbinding: fix parsing of links at the very end of the help text
Regression from 0ba452e8ac469b493e7cbf0ce7f9860ba2b09282.

tools/genbinding.cpp
@@ -499,15 +499,21 @@ static void formatHtmlText(std::ostream &stream, const std::string_view &text)
   for(size_t i {}; i < text.size(); ++i) {
     size_t linkStart { i };
     if(i == nextLink) {
-      while(i < text.size() - 1 && (isalnum(text[i]) || text[i] == '_'))
+      while(i < text.size() && (isalnum(text[i]) || text[i] == '_'))
         ++i;
+
       const std::string_view linkTo { &text[linkStart], i - linkStart };
       if(std::binary_search(g_funcs.begin(), g_funcs.end(), linkTo, FunctionComp{}))
         stream << "<a href=\"#" << linkTo << "\">" << linkTo << "</a>";
       else
         stream << linkTo;
-      nextLink = text.find("ImGui_", i);
+
+      if(i == text.size())
+        break;
+      else
+        nextLink = text.find("ImGui_", i);
     }
+
 
     switch(text[i]) {
     case '<':