cfillion/reaimgui

commit a346154c4127b87f71fe47b07160929482267414

fix visibility of popups and menus opened directly over REAPER's main window

src/context.cpp
@@ -108,7 +108,6 @@ Context::Context(const char *label, const int userConfigFlags)
   io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
   io.BackendFlags |= ImGuiBackendFlags_PlatformHasViewports;
   io.BackendFlags |= ImGuiBackendFlags_HasMouseHoveredViewport;
-  io.ConfigViewportsNoAutoMerge = true; // disable the main viewport
   io.LogFilename = logFn.c_str();
   io.UserData = this;
 
@@ -183,6 +182,12 @@ void Context::beginFrame()
   updateSettings();
 
   ImGui::NewFrame();
+
+  // Disable hosting windows in the non-rendered main viewport.
+  // ConfigViewportsNoAutoMerge doesn't apply to popups/tooltips/menus.
+  // CanHostOtherWindows applies to everything but it's reset at every NewFrame.
+  const ImGuiPlatformIO &pio { m_imgui->PlatformIO };
+  pio.Viewports[0]->Flags &= ~ImGuiViewportFlags_CanHostOtherWindows;
 
   dragSources();
   m_dockers->drawAll();

commit 2a896d20b2b45bfd2c108638a91911e0a5f4ed7a

fix windows still merging into the main viewport during NewFrame [p=2486034]
Regression from a346154c4127b87f71fe47b07160929482267414.

src/context.cpp
@@ -183,12 +183,6 @@ void Context::beginFrame()
 
   ImGui::NewFrame();
 
-  // Disable hosting windows in the non-rendered main viewport.
-  // ConfigViewportsNoAutoMerge doesn't apply to popups/tooltips/menus.
-  // CanHostOtherWindows applies to everything but it's reset at every NewFrame.
-  const ImGuiPlatformIO &pio { m_imgui->PlatformIO };
-  pio.Viewports[0]->Flags &= ~ImGuiViewportFlags_CanHostOtherWindows;
-
   dragSources();
   m_dockers->drawAll();
 }

src/viewport.cpp
@@ -34,9 +34,9 @@ public:
   void setSize(ImVec2) override {}
   void setFocus() override {}
   bool hasFocus() const override { return false; }
-  bool isMinimized() const override { return false; }
+  bool isMinimized() const override;
   void setTitle(const char *) override {}
-  void update() override {}
+  void update() override {};
   void render(void *) override {}
   float scaleFactor() const override;
   void onChanged() override {}
@@ -160,6 +160,15 @@ MainViewport::MainViewport()
   m_viewport->PlatformUserData = this;
   m_viewport->PlatformHandle = m_hwnd;
   m_viewport->DpiScale = scaleFactor();
+}
+
+bool MainViewport::isMinimized() const
+{
+  // Disable hosting windows in the non-rendered main viewport.
+  // ConfigViewportsNoAutoMerge doesn't apply to popups/tooltips/menus.
+  // CanHostOtherWindows applies to everything but it's reset at every NewFrame.
+  // ViewportFlags_Minimized bypasses UpdateTryMergeWindowIntoHostViewport.
+  return true;
 }
 
 float MainViewport::scaleFactor() const