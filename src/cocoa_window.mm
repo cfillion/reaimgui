cfillion/reaimgui

commit 23560725a9e010887a459dff6ab4c0167812eaf3

fix first frame of every window being rendered blank

src/cocoa_window.mm
@@ -176,6 +176,10 @@ void CocoaWindow::render(void *)
     [m_gl setView:m_view];
 
   [m_gl makeCurrentContext];
+  if(m_needTexUpload) {
+    m_renderer->uploadFontTex();
+    m_needTexUpload = false;
+  }
   m_renderer->render(m_viewport);
   [m_gl flushBuffer];
   [NSOpenGLContext clearCurrentContext];
@@ -199,13 +203,6 @@ void CocoaWindow::setIME(ImGuiPlatformImeData *data)
   pos.y -= data->InputLineHeight;
 
   [m_inputView setImePosition:NSMakePoint(pos.x, pos.y)];
-}
-
-void CocoaWindow::uploadFontTex()
-{
-  [m_gl makeCurrentContext];
-  m_renderer->uploadFontTex();
-  [NSOpenGLContext clearCurrentContext];
 }
 
 std::optional<LRESULT> CocoaWindow::handleMessage

src/gdk_window.cpp
@@ -258,6 +258,11 @@ void GDKWindow::render(void *)
 {
   gdk_gl_context_make_current(m_gl);
 
+  if(m_needTexUpload) {
+    m_renderer->uploadFontTex();
+    m_needTexUpload = false;
+  }
+
   const bool softwareBlit { isDocked() };
   m_renderer->render(m_viewport, softwareBlit);
 
@@ -340,13 +345,6 @@ void GDKWindow::setIME(ImGuiPlatformImeData *data)
   area.width = 0;
   area.height = data->InputLineHeight;
   gtk_im_context_set_cursor_location(m_ime, &area);
-}
-
-void GDKWindow::uploadFontTex()
-{
-  gdk_gl_context_make_current(m_gl);
-  m_renderer->uploadFontTex();
-  gdk_gl_context_clear_current();
 }
 
 static unsigned int unmangleSwellChar(WPARAM wParam, LPARAM lParam)

src/win32_window.cpp
@@ -311,6 +311,10 @@ void Win32Window::update()
 void Win32Window::render(void *)
 {
   wglMakeCurrent(m_dc, m_gl);
+  if(m_needTexUpload) {
+    m_renderer->uploadFontTex();
+    m_needTexUpload = false;
+  }
   m_renderer->render(m_viewport);
   SwapBuffers(m_dc);
   wglMakeCurrent(nullptr, nullptr);
@@ -343,13 +347,6 @@ void Win32Window::setIME(ImGuiPlatformImeData *data)
 
     ImmReleaseContext(m_hwnd.get(), ime);
   }
-}
-
-void Win32Window::uploadFontTex()
-{
-  wglMakeCurrent(m_dc, m_gl);
-  m_renderer->uploadFontTex();
-  wglMakeCurrent(nullptr, nullptr);
 }
 
 std::optional<LRESULT> Win32Window::handleMessage

src/window.cpp
@@ -117,8 +117,8 @@ LRESULT CALLBACK Window::proc(HWND handle, const unsigned int msg,
 }
 
 Window::Window(ImGuiViewport *viewport, DockerHost *dockerHost)
-  : Viewport { viewport }, m_dockerHost { dockerHost },
-    m_accel { &translateAccel, true, this },
+  : Viewport { viewport }, m_needTexUpload { true },
+    m_dockerHost { dockerHost }, m_accel { &translateAccel, true, this },
     m_accelReg { "accelerator", &m_accel }, m_previousScale { 0.f },
     m_fontTexVersion { -1 }, m_mouseDown { 0 }, m_noFocus { false }
 {
@@ -193,7 +193,7 @@ void Window::onChanged()
 
   const int fontTexVersion { m_ctx->fonts().setScale(m_viewport->DpiScale) };
   if(scaleChanged || fontTexVersion != m_fontTexVersion) {
-    uploadFontTex();
+    m_needTexUpload = true;
     m_fontTexVersion = fontTexVersion;
   }
 }

src/window.hpp
@@ -63,12 +63,12 @@ protected:
   void createSwellDialog();
   HWND parentHandle();
 
-  virtual void uploadFontTex() = 0;
   virtual std::optional<LRESULT> handleMessage(unsigned int msg, WPARAM, LPARAM) = 0;
   virtual int handleAccelerator(MSG *);
 
   struct WindowDeleter { void operator()(HWND); };
   std::unique_ptr<std::remove_pointer_t<HWND>, WindowDeleter> m_hwnd;
+  bool m_needTexUpload;
 
 private:
   static int hwndInfo(HWND, INT_PTR type);

src/cocoa_window.hpp
@@ -44,7 +44,6 @@ public:
   float scaleFactor() const override;
   void setIME(ImGuiPlatformImeData *) override;
 
-  void uploadFontTex() override;
   std::optional<LRESULT> handleMessage
     (const unsigned int msg, WPARAM wParam, LPARAM) override;
   int handleAccelerator(MSG *) override;

src/gdk_window.hpp
@@ -49,7 +49,6 @@ public:
   float scaleFactor() const override { return globalScaleFactor(); }
   void setIME(ImGuiPlatformImeData *) override;
 
-  void uploadFontTex() override;
   std::optional<LRESULT> handleMessage
     (const unsigned int msg, WPARAM wParam, LPARAM) override;
 

src/win32_window.hpp
@@ -41,7 +41,6 @@ public:
   float scaleFactor() const override;
   void setIME(ImGuiPlatformImeData *) override;
 
-  void uploadFontTex() override;
   std::optional<LRESULT> handleMessage
     (const unsigned int msg, WPARAM wParam, LPARAM) override;
 

commit 8733fce09afed02b3d81545227a4e781c707c7c5

fix corrupted font texture in contexts with windows having different DPI scaling
regression from 23560725a9e010887a459dff6ab4c0167812eaf3

src/cocoa_window.mm
@@ -202,7 +202,7 @@ void CocoaWindow::render(void *)
     // NSViewFrameDidChangeNotification or WM_SIZE aren't sent
     [m_gl update];
 
-    m_renderer->uploadFontTex();
+    m_renderer->uploadFontTex(m_fontAtlas);
     m_needTexUpload = false;
   }
   m_renderer->render(m_viewport);

src/font.cpp
@@ -116,7 +116,7 @@ void FontList::update()
   }
 }
 
-int FontList::setScale(const float scale)
+ImFontAtlas *FontList::setScale(const float scale)
 {
   ImGuiIO &io { ImGui::GetIO() };
 
@@ -133,7 +133,7 @@ int FontList::setScale(const float scale)
   if(atlasChanged)
     migrateActiveFonts();
 
-  return m_version;
+  return atlas.get();
 }
 
 void FontList::build(const float scale)

src/font.hpp
@@ -71,7 +71,7 @@ public:
   void keepAliveAll();
   void update();
   int texVersion() const { return m_version; }
-  int setScale(float scale); // returns the current texture version
+  ImFontAtlas *setScale(float scale);
   Font *get(ImFont *) const;
   ImFont *instanceOf(Font *) const;
 

src/gdk_window.cpp
@@ -283,7 +283,7 @@ void GDKWindow::render(void *)
   gdk_gl_context_make_current(m_gl);
 
   if(m_needTexUpload) {
-    m_renderer->uploadFontTex();
+    m_renderer->uploadFontTex(m_fontAtlas);
     m_needTexUpload = false;
   }
 

src/opengl_renderer.cpp
@@ -152,19 +152,18 @@ void OpenGLRenderer::initShaders()
   glUseProgram(m_program);
 }
 
-void OpenGLRenderer::uploadFontTex()
+void OpenGLRenderer::uploadFontTex(ImFontAtlas *atlas)
 {
-  ImGuiIO &io { ImGui::GetIO() };
   unsigned char *pixels;
   int width, height;
-  io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+  atlas->GetTexDataAsRGBA32(&pixels, &width, &height);
 
   glBindTexture(GL_TEXTURE_2D, m_textures[FontTex]);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
-  io.Fonts->SetTexID(FontTex);
+  atlas->SetTexID(FontTex);
 }
 
 OpenGLRenderer::~OpenGLRenderer()

src/opengl_renderer.hpp
@@ -21,6 +21,7 @@
 #include <array>
 
 struct ImDrawData;
+struct ImFontAtlas;
 struct ImGuiViewport;
 
 class OpenGLRenderer {
@@ -31,7 +32,7 @@ public:
   OpenGLRenderer();
   ~OpenGLRenderer();
 
-  void uploadFontTex();
+  void uploadFontTex(ImFontAtlas *);
   void render(ImGuiViewport *, bool flip = false);
 
 private:

src/win32_window.cpp
@@ -356,7 +356,7 @@ void Win32Window::render(void *)
 {
   wglMakeCurrent(m_dc, m_gl);
   if(m_needTexUpload) {
-    m_renderer->uploadFontTex();
+    m_renderer->uploadFontTex(m_fontAtlas);
     m_needTexUpload = false;
   }
   m_renderer->render(m_viewport);

src/window.cpp
@@ -145,6 +145,8 @@ Window::Window(ImGuiViewport *viewport, DockerHost *dockerHost)
   if(ImGuiWindow *userWindow { viewportPrivate->Window })
     m_noFocus = userWindow->Flags & ImGuiWindowFlags_NoFocusOnAppearing;
 
+  m_fontAtlas = ImGui::GetIO().Fonts;
+
   // Cannot initialize m_hwnd during construction due to handleMessage being
   // virtual. This task is delayed to created() called once fully constructed.
 }
@@ -202,7 +204,8 @@ void Window::onChanged()
   const bool scaleChanged { m_previousScale != m_viewport->DpiScale };
   m_previousScale = m_viewport->DpiScale;
 
-  const int fontTexVersion { m_ctx->fonts().setScale(m_viewport->DpiScale) };
+  m_fontAtlas = m_ctx->fonts().setScale(m_viewport->DpiScale);
+  const int fontTexVersion { m_ctx->fonts().texVersion() };
   if(scaleChanged || fontTexVersion != m_fontTexVersion) {
     m_needTexUpload = true;
     m_fontTexVersion = fontTexVersion;

src/window.hpp
@@ -29,6 +29,7 @@
 #endif
 
 class DockerHost;
+struct ImFontAtlas;
 typedef int ImGuiMouseButton;
 
 class Window : public Viewport {
@@ -78,6 +79,7 @@ protected:
   struct WindowDeleter { void operator()(HWND); };
   std::unique_ptr<std::remove_pointer_t<HWND>, WindowDeleter> m_hwnd;
   bool m_needTexUpload;
+  ImFontAtlas *m_fontAtlas;
 
 private:
   static int hwndInfo(HWND, INT_PTR type);