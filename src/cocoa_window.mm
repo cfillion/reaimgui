cfillion/reaimgui

commit 12fa1cdd68f44c57e52e5fbe76059b31d5eb9c21

enable window transparency with WindowFlags_NoBackground

src/cocoa_window.mm
@@ -76,6 +76,14 @@ void CocoaWindow::create()
   if(!m_gl)
     throw backend_error { "failed to initialize OpenGL 3.2 core context" };
 
+  // enable transparency
+  if(!isDocked()) {
+    [window setOpaque:NO];
+    [m_view setWantsLayer:YES]; // required to be transparent before resizing
+    GLint value { 0 };
+    [m_gl setValues:&value forParameter:NSOpenGLCPSurfaceOpacity];
+  }
+
   [m_gl makeCurrentContext];
   m_renderer = new OpenGLRenderer;
   [NSOpenGLContext clearCurrentContext];
@@ -165,6 +173,13 @@ void CocoaWindow::update()
     const bool topmost { (m_viewport->Flags & ImGuiViewportFlags_TopMost) != 0 };
     SWELL_SetWindowWantRaiseAmt(m_hwnd.get(), topmost);
   }
+
+  // disable shadows under the window when WindowFlags_NoBackground is set
+  // (shadows wouldn't be updated along with the contents until next resize)
+  if(diff & ImGuiViewportFlags_NoRendererClear) {
+    const bool opaque { (m_viewport->Flags & ImGuiViewportFlags_NoRendererClear) != 0 };
+    [window setHasShadow:opaque];
+  }
 }
 
 void CocoaWindow::render(void *)
@@ -208,11 +223,8 @@ std::optional<LRESULT> CocoaWindow::handleMessage
   (const unsigned int msg, WPARAM wParam, LPARAM)
 {
   switch(msg) {
-  case WM_PAINT: // update size if it changed while we were docked & inactive
   case WM_SIZE:
-    [m_gl update];
-    if(m_viewport->DrawData && m_viewport->DrawData->Valid)
-      render(nullptr);
+    [m_gl update]; // update size if it changed while we were docked & inactive
     break; // continue handling WM_SIZE in CocoaWindow::proc
   }
 

api/style.cpp
@@ -202,7 +202,7 @@ DEFINE_API(void, PopStyleColor, (ImGui_Context*,ctx)
 // ImGuiCol
 DEFINE_ENUM(ImGui, Col_Text,                  "");
 DEFINE_ENUM(ImGui, Col_TextDisabled,          "");
-DEFINE_ENUM(ImGui, Col_WindowBg,              "Background of normal windows.");
+DEFINE_ENUM(ImGui, Col_WindowBg,              "Background of normal windows. The alpha channel is ignored, use ImGui_WindowFlags_NoBackground for transparent windows instead.");
 DEFINE_ENUM(ImGui, Col_ChildBg,               "Background of child windows.");
 DEFINE_ENUM(ImGui, Col_PopupBg,               "Background of popups, menus, tooltips windows.");
 DEFINE_ENUM(ImGui, Col_Border,                "");

src/CMakeLists.txt
@@ -25,7 +25,7 @@ if(WIN32)
     win32_window.cpp
   )
 
-  target_link_libraries(src Imm32)
+  target_link_libraries(src Dwmapi Imm32)
 
   find_package(gl3w CONFIG REQUIRED)
   target_link_libraries(src unofficial::gl3w::gl3w)

src/gdk_window.cpp
@@ -52,6 +52,7 @@ GDKWindow::GDKWindow(ImGuiViewport *viewport, DockerHost *dockerHost)
 void GDKWindow::create()
 {
   createSwellDialog();
+  SetProp(m_hwnd.get(), "SWELLGdkAlphaChannel", reinterpret_cast<HANDLE>(1));
   SetWindowLongPtr(m_hwnd.get(), GWL_EXSTYLE, WS_EX_ACCEPTFILES);
 
   // WS_CHILD does gdk_window_set_override_redirect(true)

src/opengl_renderer.cpp
@@ -189,7 +189,7 @@ void OpenGLRenderer::render(ImGuiViewport *viewport, const bool flip)
     return;
 
   if(!(viewport->Flags & ImGuiViewportFlags_NoRendererClear)) {
-    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glClearColor(0.f, 0.f, 0.f, 0.f); // premultiplied alpha
     glClear(GL_COLOR_BUFFER_BIT);
   }
 

src/win32_window.cpp
@@ -28,6 +28,9 @@
 #include <GL/wglext.h>
 #include <reaper_plugin_secrets.h>
 #include <ShellScalingApi.h> // GetDpiForMonitor
+#define GetThemeColor Win32_GetThemeColor // solve conflict with REAPER API
+#include <dwmapi.h> // Dwm* functions for compositing
+#undef GetThemeColor
 
 #include "win32_droptarget.ipp"
 
@@ -153,6 +156,15 @@ void Win32Window::create()
 
   // disable IME by default
   ImmAssociateContextEx(m_hwnd.get(), nullptr, 0);
+
+  // enable compositing for transparency
+  HRGN region { CreateRectRgn(0, 0, -1, -1) };
+  DWM_BLURBEHIND bb {};
+  bb.dwFlags = DWM_BB_ENABLE | DWM_BB_BLURREGION;
+  bb.fEnable = true;
+  bb.hRgnBlur = region; // no actual blur/shadow
+  DwmEnableBlurBehindWindow(m_hwnd.get(), &bb);
+  DeleteObject(region);
 }
 
 Win32Window::~Win32Window()
@@ -186,7 +198,8 @@ void Win32Window::initPixelFormat()
   pfd.nVersion = 1;
   pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL;
   pfd.iPixelType = PFD_TYPE_RGBA;
-  pfd.cColorBits = 24;
+  pfd.cAlphaBits = pfd.cBlueBits = pfd.cGreenBits = pfd.cRedBits = 8;
+  pfd.cColorBits = pfd.cRedBits + pfd.cGreenBits + pfd.cBlueBits + pfd.cAlphaBits;
 
   if(!SetPixelFormat(m_dc, ChoosePixelFormat(m_dc, &pfd), &pfd)) {
     ReleaseDC(m_hwnd.get(), m_dc);

commit c1284fa036417bf4a171c56776c0bba2a6eb266b

cocoa: fix rescaling when moving windows to a monitor using a different DPI setting
regression from 12fa1cdd68f44c57e52e5fbe76059b31d5eb9c21

src/cocoa_window.mm
@@ -198,6 +198,10 @@ void CocoaWindow::render(void *)
 
   [m_gl makeCurrentContext];
   if(m_needTexUpload) {
+    // resize macOS's GL objects when DPI changes (eg. moving to another screen)
+    // NSViewFrameDidChangeNotification or WM_SIZE aren't sent
+    [m_gl update];
+
     m_renderer->uploadFontTex();
     m_needTexUpload = false;
   }