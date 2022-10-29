cfillion/reaimgui

commit caa8d693e29121a391995fb26878582fc872f2f1

cocoa: give key focus when switching from another docker tab
WM_SETFOCUS was added in justinfrankel/WDL@20057d485a4ee345fb8db6c846f124672b300f7c

src/cocoa_window.mm
@@ -108,9 +108,6 @@ void CocoaWindow::show()
   Window::show();
   [m_gl setView:m_view];
   [m_eventHandler watchView:m_view];
-
-  if(!(m_viewport->Flags & ImGuiViewportFlags_NoFocusOnAppearing))
-    [[m_view window] makeFirstResponder:m_inputView];
 }
 
 void CocoaWindow::setPosition(ImVec2 pos)
@@ -157,6 +154,10 @@ void CocoaWindow::setAlpha(const float alpha)
 
 void CocoaWindow::update()
 {
+  static bool no_wm_setfocus { atof(GetAppVersion()) < 6.53 };
+  if(no_wm_setfocus && GetFocus() == m_hwnd.get())
+    setFocus();
+
   if(isDocked())
     return;
 
@@ -244,6 +245,17 @@ std::optional<LRESULT> CocoaWindow::handleMessage
   case WM_SIZE:
     [m_gl update]; // update size if it changed while we were docked & inactive
     break; // continue handling WM_SIZE in CocoaWindow::proc
+  case WM_SETFOCUS: // REAPER v6.53+
+    // Redirect focus to the input view after m_view gets it.
+    // WM_SETFOCUS is sent from becomeFirstResponder,
+    // m_view will gain focus right after this returns.
+    //
+    // Both makeFirstResponder and making the window key are required
+    // for receiving key events right away without a click
+    if(!(m_viewport->Flags & ImGuiViewportFlags_NoFocusOnAppearing) ||
+        IsWindowVisible(m_hwnd.get()))
+      dispatch_async(dispatch_get_main_queue(), ^{ setFocus(); });
+    break;
   }
 
   return std::nullopt;

src/cocoa_inputview.mm
@@ -188,7 +188,13 @@ static int translateKeyCode(NSEvent *event)
 
 - (BOOL)resignFirstResponder
 {
-  // called if docked when losing focus to another control
+  // Invoked when losing focus to another control in the same OS window
+  // Calling updateFocus later because at this time focus has yet to be transferred
+  dispatch_async(dispatch_get_main_queue(), ^{
+    if([self superview]) // the viewport may have been destroyed in the meantime
+      m_window->context()->updateFocus();
+  });
+
   return YES;
 }
 

src/context.cpp
@@ -128,6 +128,9 @@ Context::~Context()
 
   if(m_inFrame)
     endFrame(false);
+
+  // destroy windows while this and m_imgui are still valid
+  ImGui::DestroyPlatformWindows();
 }
 
 void Context::ContextDeleter::operator()(ImGuiContext *imgui)
@@ -221,8 +224,8 @@ bool Context::endFrame(const bool render) try
     ImGui::EndFrame();
 
   ImGui::UpdatePlatformWindows();
-#ifndef _WIN32
-  // WM_KILLFOCUS is incomplete/missing on macOS/Linux with SWELL
+#if !defined(_WIN32) && !defined(__APPLE__)
+  // WM_KILLFOCUS/WM_ACTIVATE+WA_INACTIVE are incomplete or missing in SWELL
   updateFocus();
 #endif
 
@@ -263,7 +266,6 @@ void Context::updateCursor()
 
   if(m_imgui->IO.ConfigFlags & ImGuiConfigFlags_NoMouseCursorChange)
     return;
-
 
   // TODO
   // io.MouseDrawCursor (ImGui-drawn cursor)

commit 456dece8a1ab8f2ee91d32b6765418726e60fc21

cocoa: fix noninitial windows not gaining focus
regression from caa8d693e29121a391995fb26878582fc872f2f1

src/cocoa_window.mm
@@ -138,7 +138,12 @@ void CocoaWindow::setFocus()
 
 bool CocoaWindow::hasFocus() const
 {
-  return GetFocus() == (__bridge HWND)m_inputView;
+  // m_hwnd temporarily has focus between ShowWindow and WM_SETFOCUS
+  // returning true here in this case is important, otherwise
+  // Context::updateFocus would misdetect no active windows when it's called
+  // from EventHandler::windowDidResignKey
+  HWND focus { GetFocus() };
+  return focus == (__bridge HWND)m_inputView || focus == m_hwnd.get();
 }
 
 void CocoaWindow::setTitle(const char *title)
@@ -154,6 +159,7 @@ void CocoaWindow::setAlpha(const float alpha)
 
 void CocoaWindow::update()
 {
+  // restore keyboard focus to the input view when switching to our docker tab
   static bool no_wm_setfocus { atof(GetAppVersion()) < 6.53 };
   if(no_wm_setfocus && GetFocus() == m_hwnd.get())
     setFocus();