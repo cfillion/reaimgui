299747a7a9eb54cacd48983de109399d215479fb
cfillion <cfillion@users.noreply.github.com>
2022-11-25 17:17:03 -0500

    metal: enable texture tiling (and make it explicit in opengl)
    D3D10_TEXTURE_ADDRESS_WRAP is already set in the d3d10 renderer.

--- src/opengl_renderer.cpp
+++ src/opengl_renderer.cpp
@@ -124,6 +124,8 @@ void OpenGLRenderer::Shared::textureCommand(const TextureCmd &cmd)
       glBindTexture(GL_TEXTURE_2D, m_textures[cmd.offset + i]);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
     }
     break;
