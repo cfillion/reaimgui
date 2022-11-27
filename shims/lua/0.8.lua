44976afe405a086115831ee03716e1a989763a01
cfillion <cfillion@users.noreply.github.com>
2022-11-27 14:53:06 -0500

    add Key_Mod to v0.8's shims

--- shims/lua/0.8.lua
+++ shims/lua/0.8.lua
@@ -1,9 +1,13 @@
--- ModFlags to Mod rename
+-- ModFlags and Key_Mod to Mod rename
 reaper.ImGui_ModFlags_None  = reaper.ImGui_Mod_None
 reaper.ImGui_ModFlags_Ctrl  = reaper.ImGui_Mod_Ctrl
 reaper.ImGui_ModFlags_Shift = reaper.ImGui_Mod_Shift
 reaper.ImGui_ModFlags_Alt   = reaper.ImGui_Mod_Alt
 reaper.ImGui_ModFlags_Super = reaper.ImGui_Mod_Super
+reaper.ImGui_Key_ModCtrl  = reaper.ImGui_Mod_Ctrl
+reaper.ImGui_Key_ModShift = reaper.ImGui_Mod_Shift
+reaper.ImGui_Key_ModAlt   = reaper.ImGui_Mod_Alt
+reaper.ImGui_Key_ModSuper = reaper.ImGui_Mod_Super
 
 -- new generic object attachment functions
 reaper.ImGui_AttachFont = reaper.ImGui_Attach
