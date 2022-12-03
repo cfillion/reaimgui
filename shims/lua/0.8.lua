d354e13074eeccc92b70cacb5b17e75ec39e25e0
cfillion <cfillion@users.noreply.github.com>
2022-12-03 08:25:02 -0500

    add shims to restore pre-0.8 values for GetKeyMods [p=2619098]

--- shims/lua/0.8.lua
+++ shims/lua/0.8.lua
@@ -1,13 +1,26 @@
 -- ModFlags and Key_Mod to Mod rename
-reaper.ImGui_ModFlags_None  = reaper.ImGui_Mod_None
-reaper.ImGui_ModFlags_Ctrl  = reaper.ImGui_Mod_Ctrl
-reaper.ImGui_ModFlags_Shift = reaper.ImGui_Mod_Shift
-reaper.ImGui_ModFlags_Alt   = reaper.ImGui_Mod_Alt
-reaper.ImGui_ModFlags_Super = reaper.ImGui_Mod_Super
+-- the new Mod_* values are different than the old ModFlags_*
+reaper.ImGui_ModFlags_None  = function() return 0 end
+reaper.ImGui_ModFlags_Ctrl  = function() return 1 end
+reaper.ImGui_ModFlags_Shift = function() return 2 end
+reaper.ImGui_ModFlags_Alt   = function() return 4 end
+reaper.ImGui_ModFlags_Super = function() return 8 end
 reaper.ImGui_Key_ModCtrl  = reaper.ImGui_Mod_Ctrl
 reaper.ImGui_Key_ModShift = reaper.ImGui_Mod_Shift
 reaper.ImGui_Key_ModAlt   = reaper.ImGui_Mod_Alt
 reaper.ImGui_Key_ModSuper = reaper.ImGui_Mod_Super
+
+local GetKeyMods = reaper.ImGui_GetKeyMods
+function reaper.ImGui_GetKeyMods(ctx)
+  local old_mods, new_mods = 0, GetKeyMods(ctx)
+  local mods = { 'Ctrl', 'Shift', 'Alt', 'Super' }
+  for _, mod in ipairs(mods) do
+    if (new_mods & reaper['ImGui_Mod_' .. mod]()) ~= 0 then
+      old_mods = old_mods | reaper['ImGui_ModFlags_' .. mod]()
+    end
+  end
+  return old_mods
+end
 
 -- new generic object attachment functions
 reaper.ImGui_AttachFont = reaper.ImGui_Attach
