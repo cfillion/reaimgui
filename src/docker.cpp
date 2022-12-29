cfillion/reaimgui

commit 88f4a8f5f95cca1c1722cbb215042b130bd257eb

docker: reuse existing native window when dropping a window back into the same docker
Fix incorrect Direct3D surface size (and 1-frame flicker)

src/docker.cpp
@@ -242,12 +242,6 @@ void DockerHost::setAlpha(float)
 {
 }
 
-void DockerHost::update()
-{
-  if(m_window)
-    m_window->update();
-}
-
 float DockerHost::scaleFactor() const
 {
   return m_window ? m_window->scaleFactor() : 1.f;
@@ -255,18 +249,8 @@ float DockerHost::scaleFactor() const
 
 void DockerHost::onChanged()
 {
-  const bool isActive { m_docker->isActive() };
-  if(isActive ^ !!m_window) {
-    if(isActive)
-      activate();
-    else if(!ImGui::IsMouseDown(ImGuiMouseButton_Left) &&
-            !ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
-      m_viewport->PlatformHandle = nullptr;
-      if(m_window)
-        m_window->destroy();
-      m_window.reset();
-    }
-  }
+  if(!m_window && m_docker->isActive())
+    activate();
 
   ImGuiViewportP *viewport { static_cast<ImGuiViewportP *>(m_viewport) };
   if(ImGuiWindow *userWindow { viewport->Window }) {
@@ -283,6 +267,26 @@ void DockerHost::onChanged()
 
     m_window->onChanged();
   }
+}
+
+void DockerHost::update()
+{
+  if(!m_window)
+    return;
+
+  // in update() rather than onChanged() to be executed after DockSpace()
+  // handles drag/drop events, so that we don't close the docker in the frame
+  // during which a window is dropped creating a dock request
+  const ImGuiContext *ctx { ImGui::GetCurrentContext() };
+  if(!m_docker->isActive() &&
+     !ctx->MovingWindow && ctx->DockContext.Requests.Size == 0) {
+    m_viewport->PlatformHandle = nullptr;
+    m_window->destroy();
+    m_window.reset();
+    return;
+  }
+
+  m_window->update();
 }
 
 void DockerHost::setIME(ImGuiPlatformImeData *data)

commit eae8872e8cdf3018e4b568fd2cf00f5874dd7acf

docker: fix windows becoming invisible after undocking via API from a split
Regression from 88f4a8f5f95cca1c1722cbb215042b130bd257eb

src/docker.cpp
@@ -156,7 +156,7 @@ Docker *DockerList::findByViewport(const ImGuiViewport *viewport)
 }
 
 DockerHost::DockerHost(Docker *docker, ImGuiViewport *viewport)
-  : Viewport { viewport }, m_docker { docker }
+  : Viewport { viewport }, m_docker { docker }, m_resetNextFrame { false }
 {
 }
 
@@ -248,6 +248,13 @@ float DockerHost::scaleFactor() const
 
 void DockerHost::onChanged()
 {
+  if(m_resetNextFrame) {
+    m_viewport->PlatformHandle = nullptr;
+    m_window->destroy();
+    m_window.reset();
+    m_resetNextFrame = false;
+  }
+
   if(!m_window && m_docker->isActive())
     activate();
 
@@ -273,19 +280,20 @@ void DockerHost::update()
   if(!m_window)
     return;
 
-  // in update() rather than onChanged() to be executed after DockSpace()
+  m_window->update();
+
+  // In update() rather than onChanged() to be executed after DockSpace()
   // handles drag/drop events, so that we don't close the docker in the frame
   // during which a window is dropped creating a dock request
+  //
+  // The platform window must be reset only in onChanged() however, otherwise
+  // windows would become invisible in the following scenario:
+  // 1) dock 2) move to split 3) undock via API = windows stays attached to the
+  // dock's viewport if m_window.reset() is done here in update().
   const ImGuiContext *ctx { ImGui::GetCurrentContext() };
   if(!m_docker->isActive() &&
-     !ctx->MovingWindow && ctx->DockContext.Requests.Size == 0) {
-    m_viewport->PlatformHandle = nullptr;
-    m_window->destroy();
-    m_window.reset();
-    return;
-  }
-
-  m_window->update();
+     !ctx->MovingWindow && ctx->DockContext.Requests.Size == 0)
+    m_resetNextFrame = true;
 }
 
 void DockerHost::setIME(ImGuiPlatformImeData *data)

src/docker.hpp
@@ -92,6 +92,7 @@ private:
 
   Docker *m_docker;
   std::unique_ptr<Window> m_window;
+  bool m_resetNextFrame;
 };
 
 #endif