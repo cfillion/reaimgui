cfillion/reaimgui

commit 6a2aaedb87723603f829512f1c9e238834da631a

reuse WithinFrameScope instead of own duplicate variable

src/context.cpp
@@ -85,7 +85,7 @@ Context *Context::current()
 }
 
 Context::Context(const char *label, const int userConfigFlags)
-  : m_inFrame { false }, m_dropFrameCount { DropState_None }, m_cursor {},
+  : m_dropFrameCount { DropState_None }, m_cursor {},
     m_lastFrame       { decltype(m_lastFrame)::clock::now()                },
     m_name            { label, ImGui::FindRenderedTextEnd(label)           },
     m_iniFilename     { generateIniFilename(label)                         },
@@ -129,7 +129,7 @@ Context::~Context()
 {
   setCurrent();
 
-  if(m_inFrame)
+  if(m_imgui->WithinFrameScope)
     endFrame(false);
 
   // destroy windows while this and m_imgui are still valid
@@ -153,7 +153,7 @@ void Context::setUserConfigFlags(const int userFlags)
 
 void Context::assertOutOfFrame()
 {
-  if(m_inFrame)
+  if(m_imgui->WithinFrameScope)
     throw reascript_error { "cannot modify font texture: a frame has already begun" };
 }
 
@@ -189,7 +189,7 @@ void Context::detach(Resource *obj)
 
 bool Context::heartbeat()
 {
-  if(m_inFrame) {
+  if(m_imgui->WithinFrameScope) {
     if(!endFrame(true))
       return false;
 
@@ -215,7 +215,7 @@ void Context::setCurrent()
 
 bool Context::beginFrame() try
 {
-  assert(!m_inFrame);
+  assert(!m_imgui->WithinFrameScope);
 
   Platform::updateMonitors(); // TODO only if changed
   m_fonts->update(); // uses the monitor list
@@ -230,7 +230,6 @@ bool Context::beginFrame() try
   updateSettings();
 
   ImGui::NewFrame();
-  m_inFrame = true;
 
   dragSources();
   m_dockers->drawAll();
@@ -246,7 +245,7 @@ bool Context::enterFrame()
 {
   setCurrent();
 
-  if(m_inFrame)
+  if(m_imgui->WithinFrameScope)
     return true;
   else
     return beginFrame();
@@ -254,8 +253,6 @@ bool Context::enterFrame()
 
 bool Context::endFrame(const bool render) try
 {
-  m_inFrame = false;
-
   setCurrent();
 
   if(render) {

src/context.hpp
@@ -108,7 +108,6 @@ private:
   void dragSources();
   void clearFocus();
 
-  bool m_inFrame;
   int m_dropFrameCount;
   HCURSOR m_cursor;
   std::chrono::time_point<std::chrono::steady_clock> m_lastFrame; // monotonic

commit 18c5db785fa11d57a5632e19bcb3cca8535df17e

fix duplicate error reports when failing to end a frame
Regression from 6a2aaedb87723603f829512f1c9e238834da631a (v0.8.2)

src/context.cpp
@@ -277,6 +277,8 @@ bool Context::endFrame(const bool render) try
 }
 catch(const imgui_error &e) {
   Error::report(this, e);
+  // don't retry EndFrame and error out again during destruction
+  m_imgui->WithinFrameScope = false;
   return false;
 }
 catch(const backend_error &e) {