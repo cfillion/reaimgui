9928c58ca5b1a3c53f1202ece5179597678bd471
cfillion <cfillion@users.noreply.github.com>
2023-08-27 12:46:06 -0400

    metal: fix compatibility with macOS 10.11-10.13

--- tools/genmetalshader.sh
+++ tools/genmetalshader.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -o pipefail
-xcrun -sdk macosx metal -c "$1" -o - |
-xcrun -sdk macosx metallib -    -o - |
+xcrun -sdk macosx metal -c "$1" -o - \
+  ${MACOSX_DEPLOYMENT_TARGET:+-mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET} |
+xcrun -sdk macosx metallib - -o - |
 xxd -i > "$2"
