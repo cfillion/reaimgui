cfillion/reaimgui

commit 304485cf2b3da51532e4ff628b1fd19e4ecf117c

implement error handling in the C++ bindings

api/callconv.hpp
@@ -83,7 +83,7 @@ struct Safe<fn, name>
 {
   static R invoke(Args... args) noexcept
   try {
-    // TODO: API::clearError() for C++, clearContext for correct destruction?
+    API::clearError();
     return std::invoke(fn, args...);
   }
   catch(const imgui_error &e) { // TODO: recoverable_error base class

src/api.cpp
@@ -27,6 +27,7 @@
 using namespace API;
 
 static eel_function_table g_eelFuncs;
+static std::string g_lastError[2];
 
 using CallableMap = std::unordered_map<std::string_view, Callable *>;
 
@@ -304,16 +305,32 @@ VerNum API::version()
   return latestVersion();
 }
 
+void API::clearError()
+{
+  std::swap(g_lastError[0], g_lastError[1]);
+  g_lastError[0].clear();
+#ifndef NDEBUG
+  Context::clearCurrent();
+#endif
+}
+
+const char *API::lastError()
+{
+  return g_lastError[1].empty() ? nullptr : &g_lastError[1][1];
+}
+
 // REAPER 6.29+ uses the '!' prefix to abort the calling Lua script's execution
 void API::handleError(const char *fnName, const reascript_error &e)
 {
-  ReaScriptError(std::format("!ImGui_{}: {}", fnName, e.what()).c_str());
+  g_lastError[0] = std::format("!" API_PREFIX "{}: {}", fnName, e.what());
+  ReaScriptError(g_lastError[0].c_str());
 }
 
 void API::handleError(const char *fnName, const imgui_error &e)
 {
-  ReaScriptError(std::format(
-    "!ImGui_{}: ImGui assertion failed: {}", fnName, e.what()).c_str());
+  g_lastError[0] = std::format("!" API_PREFIX "{}: ImGui assertion failed: {}", fnName, e.what());
+  ReaScriptError(g_lastError[0].c_str());
 
+  assert(Context::current());
   delete Context::current();
 }

src/api.hpp
@@ -27,6 +27,9 @@ namespace API {
   void setup();
   void teardown();
   VerNum version();
+
+  void clearError();
+  const char *lastError();
   void handleError(const char *fnName, const reascript_error &);
   void handleError(const char *fnName, const imgui_error &);
 

api/internal.cpp
@@ -31,7 +31,7 @@ static void assertVersion(const VerNum requested)
     return;
 
   throw reascript_error {
-    "ReaImGui {} is too old (action requires {} or newer)",
+    "ReaImGui {} is too old (action requires API {} or newer)",
     REAIMGUI_VERSION, requested.toString()
   };
 }
@@ -43,7 +43,13 @@ DO_NOT_USE)
   assertVersion(vernum);
   if(auto callable { API::Callable::lookup(vernum, symbol_name) })
     return callable->safeImpl();
-  return nullptr;
+  throw reascript_error { "function '{}' not found or missing shim for v{}",
+    symbol_name, vernum.toString() };
+}
+
+API_FUNC(0_9, const char*, _geterr, NO_ARGS, DO_NOT_USE)
+{
+  return API::lastError();
 }
 
 API_FUNC(0_9, void, _init, (char*,API_RWBIG(buf))(int,API_RWBIG_SZ(buf)), DO_NOT_USE)

examples/hello_world.cpp
@@ -54,8 +54,14 @@ constexpr const char *g_name { "ReaImGui C++ example" };
 static int g_actionId;
 std::unique_ptr<Example> Example::s_inst;
 
+static void reportError(const ImGui_Error &e)
+{
+  ShowMessageBox(e.what(), g_name, 0);
+}
+
 Example::Example()
-  : m_ctx {}, m_click_count {}, m_text { "The quick brown fox jumps over the lazy dog" }
+  : m_ctx {}, m_click_count {},
+    m_text { "The quick brown fox jumps over the lazy dog" }
 {
   ImGui::init(plugin_getapi);
   m_ctx = ImGui::CreateContext(g_name);
@@ -68,16 +74,24 @@ Example::~Example()
 }
 
 void Example::start()
-{
+try {
   if(s_inst)
     ImGui::SetNextWindowFocus(s_inst->m_ctx);
   else
     s_inst.reset(new Example);
 }
+catch(const ImGui_Error &e) {
+  reportError(e);
+  s_inst.reset();
+}
 
 void Example::loop()
-{
+try {
   s_inst->frame();
+}
+catch(const ImGui_Error &e) {
+  reportError(e);
+  s_inst.reset();
 }
 
 void Example::frame()
@@ -127,6 +141,8 @@ extern "C" REAPER_PLUGIN_DLL_EXPORT int REAPER_PLUGIN_ENTRYPOINT(
     (rec->GetFunc("plugin_getapi"));
   plugin_register = reinterpret_cast<decltype(plugin_register)>
     (rec->GetFunc("plugin_register"));
+  ShowMessageBox  = reinterpret_cast<decltype(ShowMessageBox)>
+    (rec->GetFunc("ShowMessageBox"));
 
   custom_action_register_t action { 0, "REAIMGUI_CPP", "ReaImGui C++ example" };
   g_actionId = plugin_register("custom_action", &action);

src/context.cpp
@@ -85,6 +85,11 @@ Context *Context::current()
     return nullptr;
 }
 
+void Context::clearCurrent()
+{
+  ImGui::SetCurrentContext(nullptr);
+}
+
 Context::Context(const char *label, const int userConfigFlags)
   : m_dndWasActive { false }, m_cursor {},
     m_lastFrame       { decltype(m_lastFrame)::clock::now()                },

src/context.hpp
@@ -50,6 +50,7 @@ constexpr const char *REAIMGUI_PAYLOAD_TYPE_FILES { "_FILES" };
 class Context final : public Resource {
 public:
   static Context *current();
+  static void clearCurrent();
 
   Context(const char *label, int userConfigFlags = ImGuiConfigFlags_None);
   ~Context();

tools/genbinding.cpp
@@ -251,6 +251,7 @@ static void cppBinding(std::ostream &stream)
 #ifndef REAPER_IMGUI_FUNCTIONS_H
 #define REAPER_IMGUI_FUNCTIONS_H
 
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
 
@@ -265,6 +266,10 @@ class ImGui_ListClipper;
 class ImGui_Resource;
 class ImGui_TextFilter;
 class ImGui_Viewport;
+
+struct ImGui_Error : std::runtime_error {
+  using runtime_error::runtime_error;
+};
 
 struct reaper_array;
 
@@ -287,6 +292,14 @@ namespace ImGui {
   constexpr details::nullopt_t nullopt { 0 };
 
   namespace details {
+    REAIMGUIAPI_EXTERN const char *(*last_error)() noexcept;
+
+    inline void check_error()
+    {
+      if(const char *err { last_error() })
+        throw ImGui_Error { err };
+    }
+
     template<typename T, typename E = void>
     class optional {
     public:
@@ -329,7 +342,7 @@ namespace ImGui {
       operator bool() const { return m_proc != nullptr; }
 
       template<typename... CallArgs>
-      R operator()(CallArgs... args) const noexcept
+      R operator()(CallArgs... args) const
       {
         if constexpr(sizeof...(CallArgs) < sizeof...(Args))
           return (*this)(std::forward<CallArgs>(args)..., nullopt);
@@ -344,7 +357,15 @@ namespace ImGui {
     private:
       R invoke(Args... args) const
       {
-        return m_proc(std::forward<Args>(args)...);
+        if constexpr(std::is_void_v<R>) {
+          m_proc(std::forward<Args>(args)...);
+          check_error();
+        }
+        else {
+          const R rv { m_proc(std::forward<Args>(args)...) };
+          check_error();
+          return rv;
+        }
       }
 
       Proc m_proc;
@@ -395,9 +416,10 @@ namespace ImGui {
 #ifdef REAIMGUIAPI_IMPLEMENT
 void ImGui::init(void *(*plugin_getapi)(const char *))
 {
-  void *(*get_func)(const char *v, const char *n) = reinterpret_cast<decltype(get_func)>(plugin_getapi("ImGui__getapi"));
-  if(!get_func)
-    return;
+  void *(*get_func)(const char *v, const char *n) noexcept = reinterpret_cast<decltype(get_func)>(plugin_getapi("ImGui__getapi"));
+  details::last_error = reinterpret_cast<decltype(details::last_error)>(plugin_getapi("ImGui__geterr"));
+  if(!get_func || !details::last_error)
+    throw ImGui_Error { "ReaImGui is not installed or too old" };
 )";
 
   for(const Function &func : g_funcs) {
@@ -410,7 +432,7 @@ void ImGui::init(void *(*plugin_getapi)(const char *))
     stream << "get_func(version, \"" << func.name << "\")";
     if(func.isEnum())
       stream << ')';
-    stream << ';';
+    stream << "; details::check_error();";
   }
 
   stream << R"(

commit d0dd5548c55e6beb56cecdf3a2220c40bf29ef63

fix EEL callbacks always thinking an error ocurred
Regression from 304485cf2b3da51532e4ff628b1fd19e4ecf117c

api/callback.hpp
@@ -35,7 +35,7 @@ public:
     {
       // prevent use-after-free if the context got destroyed during
       // this or a previous data access
-      return !!s_data && Resource::isValid(Context::current());
+      return !!s_data && !API::lastError();
     }
 
     Data *operator->() const { return Callback<Data>::s_data; }
@@ -63,7 +63,7 @@ public:
     // prevent accessing the context after it has been destructed
     // after handling an exception during execution of the callback
     // (exceptions cannot cross EEL's boundary so they're handled earlier)
-    if(!Resource::isValid(Context::current()))
+    if(API::lastError())
       throw reascript_error { "an error occurred during callback execution" };
 
     if constexpr(!std::is_void_v<T>)

api/callconv.hpp
@@ -83,10 +83,13 @@ struct Safe<fn, name>
 {
   static R invoke(Args... args) noexcept
   try {
-    API::clearError();
+    // only clear errors when first entering into an API function
+    // not when reentering (eg. EEL Function callback) so that we can still
+    // check for previous failure using API::lastError
+    API::ErrorClearer reentrant {};
     return std::invoke(fn, args...);
   }
-  catch(const imgui_error &e) { // TODO: recoverable_error base class
+  catch(const imgui_error &e) {
     API::handleError(*name, e);
     return static_cast<R>(0);
   }

api/input.cpp
@@ -322,7 +322,7 @@ local reverseAlphabet = ImGui.CreateFunctionFromEEL([[
 
 local function frame()
   rv, text = ImGui.InputText(ctx, 'Lowercase reversed', text,
-    ImGui.InputTextFlags_CallbackCharFilter(), reverseAlphabet)
+    ImGui.InputTextFlags_CallbackCharFilter, reverseAlphabet)
 end
 ```
 

src/api.cpp
@@ -308,6 +308,19 @@ void API::clearError()
 #endif
 }
 
+static unsigned int g_reentrant;
+
+ErrorClearer::ErrorClearer()
+{
+  if(!g_reentrant++)
+    clearError();
+}
+
+ErrorClearer::~ErrorClearer()
+{
+  --g_reentrant;
+}
+
 const char *API::lastError() noexcept
 {
   return g_lastError.empty() ? nullptr : &g_lastError[1];

src/api.hpp
@@ -34,6 +34,11 @@ namespace API {
   void handleError(const char *fnName, const reascript_error &);
   void handleError(const char *fnName, const imgui_error &);
 
+  struct ErrorClearer {
+    ErrorClearer();
+    ~ErrorClearer();
+  };
+
   using LineNumber = unsigned short;
   struct StoreLineNumber { StoreLineNumber(LineNumber val); };
 