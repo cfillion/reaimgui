cfillion/reaimgui

commit bf2061a741f3cac5535bfbe6b48217ec6569d875

win32: don't bring the parent window to the front when opening a viewport
Ported from ocornut/imgui@4e8c43f6b7f91d993751754dfc3089c3bc5db1b2

src/win32_window.cpp
@@ -231,7 +231,10 @@ void Win32Window::show()
   if(!isDocked() && !(m_viewport->Flags & ImGuiViewportFlags_NoDecoration))
     AttachWindowTopmostButton(m_hwnd);
 
+  // temporarily clear the parent to prevent ShowWindow from bringing it to front
+  const LONG_PTR parent { SetWindowLongPtr(m_hwnd, GWLP_HWNDPARENT, 0) };
   Window::show();
+  SetWindowLongPtr(m_hwnd, GWLP_HWNDPARENT, parent);
 
   // WS_EX_DLGMODALFRAME removes the default icon but adds a border when docked
   // Unsetting it after the window is visible disables the border (+ no icon)

commit 395bc2293de8e78761bf7a3aa9295d37e75c075a

fix docking on Windows (broken since bf2061a741f3cac5535bfbe6b48217ec6569d875)

src/docker.cpp
@@ -508,9 +508,6 @@ void DockerHost::update()
   // dropped (when a dock request is created).
   m_docker->update(true);
 
-  if(!m_window)
-    return;
-
   m_window->update();
 
   // Checking m_window->isMinimized here would prevent restoring
@@ -525,6 +522,5 @@ void DockerHost::update()
 
 void DockerHost::setIME(ImGuiPlatformImeData *data)
 {
-  if(m_window)
-    m_window->setIME(data);
+  m_window->setIME(data);
 }

src/win32_window.cpp
@@ -231,10 +231,14 @@ void Win32Window::show()
   if(!isDocked() && !(m_viewport->Flags & ImGuiViewportFlags_NoDecoration))
     AttachWindowTopmostButton(m_hwnd);
 
-  // temporarily clear the parent to prevent ShowWindow from bringing it to front
-  const LONG_PTR parent { SetWindowLongPtr(m_hwnd, GWLP_HWNDPARENT, 0) };
-  Window::show();
-  SetWindowLongPtr(m_hwnd, GWLP_HWNDPARENT, parent);
+  if(isDocked())
+    Window::show();
+  else {
+    // prevent ShowWindow from bringing the parent window to the front
+    const LONG_PTR parent { SetWindowLongPtr(m_hwnd, GWLP_HWNDPARENT, 0) };
+    Window::show();
+    SetWindowLongPtr(m_hwnd, GWLP_HWNDPARENT, parent);
+  }
 
   // WS_EX_DLGMODALFRAME removes the default icon but adds a border when docked
   // Unsetting it after the window is visible disables the border (+ no icon)