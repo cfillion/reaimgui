cfillion/reaimgui

commit 898c1b2459fb374a6d2306289831aeb940295696

ensure resource memory is never freed within a frame
1. beginFrame
2. TextureManager::clean ensures all known textures are still valid
3. Resource::heartbeat deletes unused images
4. TextureManager::update may refer to those images (eg. a window was open on Linux)
5. use-after-free!

This commit adds a grace period where the resource is deemed invalid
before being freed on the next heartbeat.

src/resource.cpp
@@ -29,8 +29,8 @@
 // Splash_GetWnd may be NULL for a brief moment after _s_splash_thread_running
 // is set internally. The misc timer may fire during this time, skipping
 // deferred scripts while still running extension callbacks (seen on Windows).
-// To workaround this, wait an extra frame before collecting unused objects.
-// [p=2450259]
+// This value must not be under 2 for working around that by waiting an extra
+// frame before collecting unused objects [p=2450259].
 constexpr unsigned char KEEP_ALIVE_FRAMES { 2 };
 
 // How many back-to-back GC frames to tolerate before complaining
@@ -59,12 +59,10 @@ static bool isDeferLoopBlocked()
   return (pauseDuringLoad && Splash_GetWnd()) || g_reentrant > 1;
 }
 
-class Resource::Timer {
-public:
+struct Resource::Timer {
   Timer();
   ~Timer();
 
-private:
   static void tick();
   static LRESULT CALLBACK mainProcOverride(HWND, unsigned int, WPARAM, LPARAM);
 };
@@ -191,7 +189,10 @@ bool Resource::heartbeat()
 
 bool Resource::isValid() const
 {
-  return true;
+  // Prevent usage of resources within their last frame of existence
+  // so that the texture manager doesn't refer to resources deleted at the
+  // frame end's heartbeat that were previously valid when the frame started.
+  return m_keepAlive;
 }
 
 void Resource::destroyAll()
@@ -203,4 +204,9 @@ void Resource::destroyAll()
 void Resource::bypassGCCheckOnce()
 {
   g_bypassGCCheckOnce = true;
+}
+
+void Resource::testHeartbeat()
+{
+  g_timer->tick();
 }

src/resource.hpp
@@ -54,6 +54,7 @@ public:
 
   static void destroyAll();
   static void bypassGCCheckOnce();
+  static void testHeartbeat();
 
   template<typename T>
   bool isInstanceOf() const
@@ -67,7 +68,7 @@ protected:
   virtual bool isValid() const;
 
 private:
-  class Timer;
+  struct Timer;
 
   static FlatSet<Resource *> g_rsx;
   static Timer *g_timer;

tests/resource_test.cpp
@@ -15,6 +15,14 @@ struct Baz : Resource {
   bool attachable(const Context *) const override { return false; }
 };
 
+struct Lifetime : Resource {
+  Lifetime(bool *alive) : m_alive { alive } { *m_alive = true; }
+  virtual ~Lifetime() { *m_alive = false; }
+  bool attachable(const Context *) const override { return false; }
+
+  bool *m_alive;
+};
+
 TEST(ResourceTest, ValidateNull) {
   auto foo { std::make_unique<Foo>() };
   EXPECT_FALSE(Resource::isValid<Foo>(nullptr));
@@ -73,4 +81,29 @@ TEST(ResourceTest, ForeachDerived) {
   unsigned int matches {};
   Resource::foreach<Foo>([&matches](const Foo *) { ++matches; });
   EXPECT_EQ(matches, 2u); // Foo + Bar (derived from Foo)
+}
+
+TEST(ResourceTest, KeepAlive) {
+  bool alive { false };
+  Lifetime res { &alive };
+  EXPECT_TRUE(alive);
+  for(int i {}; i < 1024; ++i) {
+    res.keepAlive();
+    Resource::testHeartbeat();
+    ASSERT_TRUE(alive);
+  }
+}
+
+TEST(ResourceTest, GarbageCollection) {
+  bool alive { false };
+  auto res = new Lifetime { &alive };
+  EXPECT_TRUE(alive);
+  for(int i {}; i < 2; ++i) {
+    EXPECT_TRUE(Resource::isValid(res));
+    Resource::testHeartbeat();
+    ASSERT_TRUE(alive);
+  }
+  EXPECT_FALSE(Resource::isValid(res));
+  Resource::testHeartbeat();
+  EXPECT_FALSE(alive);
 }

tests/environment.cpp
@@ -15,8 +15,10 @@ static const auto g_setup
 
 void Environment::SetUp()
 {
+  GetAppVersion   = []() { return "5.99"; };
   GetMainHwnd     = []() -> HWND { return nullptr; };
   plugin_register = [](const char *, void *) { return 0; };
+  Splash_GetWnd   = []() -> HWND { return nullptr; };
 
 #ifndef _WIN32
   GetWindowLong = [](HWND, int)           -> LONG_PTR { return 0; };

commit e5c5e23085890d39a1dbf9a7920eb2527bb987b0

fix workaround for splash screen detection mismatch
Regression from 898c1b2459fb374a6d2306289831aeb940295696

src/resource.cpp
@@ -180,7 +180,7 @@ void Resource::keepAlive()
 
 bool Resource::heartbeat()
 {
-  if(!m_keepAlive)
+  if(m_keepAlive < 0)
     return false;
 
   --m_keepAlive;
@@ -192,7 +192,7 @@ bool Resource::isValid() const
   // Prevent usage of resources within their last frame of existence
   // so that the texture manager doesn't refer to resources deleted at the
   // frame end's heartbeat that were previously valid when the frame started.
-  return m_keepAlive;
+  return m_keepAlive >= 0;
 }
 
 void Resource::destroyAll()

src/resource.hpp
@@ -73,7 +73,8 @@ private:
   static FlatSet<Resource *> g_rsx;
   static Timer *g_timer;
 
-  unsigned char m_keepAlive, m_flags;
+  signed char m_keepAlive;
+  unsigned char m_flags;
 };
 
 API_REGISTER_OBJECT_TYPE(Resource);

tests/resource_test.cpp
@@ -100,7 +100,7 @@ TEST(ResourceTest, GarbageCollection) {
   int alive {};
   auto res = new Lifetime { &alive };
   EXPECT_EQ(alive, 1);
-  for(int i {}; i < 2; ++i) {
+  for(int i {}; i <= 2; ++i) {
     EXPECT_TRUE(Resource::isValid(res));
     Resource::testHeartbeat();
     ASSERT_EQ(alive, 1);