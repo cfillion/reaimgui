cfillion/reaimgui

commit 519a4b9d655e1dd041709c8f8adf1a25c4b94f35

move API parameter decorations from the name to the type
Also fixes Combo, ListBox and GetMouseDelta not checking input pointers aren't null

api/coloredit.cpp
@@ -29,92 +29,92 @@ static void sanitizeColorEditFlags(ImGuiColorEditFlags &flags)
 }
 
 API_FUNC(0_1, bool, ColorEdit4, (Context*,ctx)
-(const char*,label)(int*,API_RW(col_rgba))
-(int*,API_RO(flags),ImGuiColorEditFlags_None),
+(const char*,label) (RW<int*>,col_rgba)
+(RO<int*>,flags,ImGuiColorEditFlags_None),
 R"(Color is in 0xRRGGBBAA or, if ColorEditFlags_NoAlpha is set, 0xXXRRGGBB
 (XX is ignored and will not be modified).)")
 {
   FRAME_GUARD;
-  assertValid(API_RW(col_rgba));
+  assertValid(col_rgba);
 
-  ImGuiColorEditFlags flags { API_RO_GET(flags) };
-  sanitizeColorEditFlags(flags);
+  ImGuiColorEditFlags clean_flags { API_GET(flags) };
+  sanitizeColorEditFlags(clean_flags);
 
-  const bool alpha { (flags & ImGuiColorEditFlags_NoAlpha) == 0 };
+  const bool alpha { (clean_flags & ImGuiColorEditFlags_NoAlpha) == 0 };
   float col[4];
-  Color(*API_RW(col_rgba), alpha).unpack(col);
-  if(ImGui::ColorEdit4(label, col, flags)) {
+  Color(*col_rgba, alpha).unpack(col);
+  if(ImGui::ColorEdit4(label, col, clean_flags)) {
     // preserves unused bits from the input integer as-is (eg. REAPER's enable flag)
-    *API_RW(col_rgba) = Color{col}.pack(alpha, *API_RW(col_rgba));
+    *col_rgba = Color{col}.pack(alpha, *col_rgba);
     return true;
   }
   return false;
 }
 
 API_FUNC(0_1, bool, ColorEdit3, (Context*,ctx)
-(const char*,label)(int*,API_RW(col_rgb))
-(int*,API_RO(flags),ImGuiColorEditFlags_None),
+(const char*,label) (RW<int*>,col_rgb)
+(RO<int*>,flags,ImGuiColorEditFlags_None),
 "Color is in 0xXXRRGGBB. XX is ignored and will not be modified.")
 {
   // Edit4 will take care of starting the frame and validating col_rgb
-  ImGuiColorEditFlags flags { API_RO_GET(flags) };
-  flags |= ImGuiColorEditFlags_NoAlpha;
-  return ColorEdit4::impl(ctx, label, API_RW(col_rgb), &flags);
+  ImGuiColorEditFlags clean_flags { API_GET(flags) };
+  clean_flags |= ImGuiColorEditFlags_NoAlpha;
+  return ColorEdit4::impl(ctx, label, col_rgb, &clean_flags);
 }
 
 API_FUNC(0_1, bool, ColorPicker4, (Context*,ctx)
-(const char*,label)(int*,API_RW(col_rgba))
-(int*,API_RO(flags),ImGuiColorEditFlags_None)(int*,API_RO(ref_col)),
+(const char*,label) (RW<int*>,col_rgba)
+(RO<int*>,flags,ImGuiColorEditFlags_None) (RO<int*>,ref_col),
 "")
 {
   FRAME_GUARD;
-  assertValid(API_RW(col_rgba));
+  assertValid(col_rgba);
 
-  ImGuiColorEditFlags flags { API_RO_GET(flags) };
-  sanitizeColorEditFlags(flags);
+  ImGuiColorEditFlags clean_flags { API_GET(flags) };
+  sanitizeColorEditFlags(clean_flags);
 
-  const bool alpha { (flags & ImGuiColorEditFlags_NoAlpha) == 0 };
+  const bool alpha { (clean_flags & ImGuiColorEditFlags_NoAlpha) == 0 };
 
   float col[4], refCol[4];
-  Color(*API_RW(col_rgba), alpha).unpack(col);
-  if(API_RO(ref_col))
-    Color(*API_RO(ref_col), alpha).unpack(refCol);
+  Color(*col_rgba, alpha).unpack(col);
+  if(ref_col)
+    Color(*ref_col, alpha).unpack(refCol);
 
-  if(ImGui::ColorPicker4(label, col, flags, API_RO(ref_col) ? refCol : nullptr)) {
+  if(ImGui::ColorPicker4(label, col, clean_flags, refCol)) {
     // preserves unused bits from the input integer as-is (eg. REAPER's enable flag)
-    *API_RW(col_rgba) = Color{col}.pack(alpha, *API_RW(col_rgba));
+    *col_rgba = Color{col}.pack(alpha, *col_rgba);
     return true;
   }
   return false;
 }
 
 API_FUNC(0_1, bool, ColorPicker3, (Context*,ctx)
-(const char*,label)(int*,API_RW(col_rgb))
-(int*,API_RO(flags),ImGuiColorEditFlags_None),
+(const char*,label) (RW<int*>,col_rgb)
+(RO<int*>,flags,ImGuiColorEditFlags_None),
 R"(Color is in 0xXXRRGGBB. XX is ignored and will not be modified.)")
 {
   // Picker4 will take care of starting the frame and validating col_rgb
-  ImGuiColorEditFlags flags { API_RO_GET(flags) };
-  flags |= ImGuiColorEditFlags_NoAlpha;
-  return ColorPicker4::impl(ctx, label, API_RW(col_rgb), &flags, nullptr);
+  ImGuiColorEditFlags clean_flags { API_GET(flags) };
+  clean_flags |= ImGuiColorEditFlags_NoAlpha;
+  return ColorPicker4::impl(ctx, label, col_rgb, &clean_flags, nullptr);
 }
 
 API_FUNC(0_1, bool, ColorButton, (Context*,ctx)
-(const char*,desc_id)(int,col_rgba)(int*,API_RO(flags),ImGuiColorEditFlags_None)
-(double*,API_RO(size_w),0.0)(double*,API_RO(size_h),0.0),
+(const char*,desc_id) (int,col_rgba) (RO<int*>,flags,ImGuiColorEditFlags_None)
+(RO<double*>,size_w,0.0) (RO<double*>,size_h,0.0),
 R"(Display a color square/button, hover for details, return true when pressed.
 Color is in 0xRRGGBBAA or, if ColorEditFlags_NoAlpha is set, 0xRRGGBB.)")
 {
   FRAME_GUARD;
 
-  ImGuiColorEditFlags flags { API_RO_GET(flags) };
-  sanitizeColorEditFlags(flags);
+  ImGuiColorEditFlags clean_flags { API_GET(flags) };
+  sanitizeColorEditFlags(clean_flags);
 
-  const bool alpha { (flags & ImGuiColorEditFlags_NoAlpha) == 0 };
+  const bool alpha { (clean_flags & ImGuiColorEditFlags_NoAlpha) == 0 };
   const Color col(col_rgba, alpha);
-  const ImVec2 size(API_RO_GET(size_w), API_RO_GET(size_h));
+  const ImVec2 size(API_GET(size_w), API_GET(size_h));
 
-  return ImGui::ColorButton(desc_id, col, flags, size);
+  return ImGui::ColorButton(desc_id, col, clean_flags, size);
 }
 
 API_FUNC(0_1, void, SetColorEditOptions, (Context*,ctx)

api/button.cpp
@@ -24,11 +24,11 @@ You may also use one of the many IsItem* functions (e.g. IsItemActive,
 IsItemHovered, etc.) to query widget state.)");
 
 API_FUNC(0_1, bool, Button, (Context*,ctx)
-(const char*,label)(double*,API_RO(size_w),0.0)(double*,API_RO(size_h),0.0),
+(const char*,label) (RO<double*>,size_w,0.0) (RO<double*>,size_h,0.0),
 "")
 {
   FRAME_GUARD;
-  return ImGui::Button(label, ImVec2(API_RO_GET(size_w), API_RO_GET(size_h)));
+  return ImGui::Button(label, ImVec2(API_GET(size_w), API_GET(size_h)));
 }
 
 API_FUNC(0_1, bool, SmallButton, (Context*,ctx)
@@ -40,17 +40,17 @@ API_FUNC(0_1, bool, SmallButton, (Context*,ctx)
 }
 
 API_FUNC(0_1, bool, InvisibleButton, (Context*,ctx)
-(const char*,str_id)(double,size_w)(double,size_h)
-(int*,API_RO(flags),ImGuiButtonFlags_None),
+(const char*,str_id) (double,size_w) (double,size_h)
+(RO<int*>,flags,ImGuiButtonFlags_None),
 R"(Flexible button behavior without the visuals, frequently useful to build
 custom behaviors using the public api (along with IsItemActive, IsItemHovered, etc.).)")
 {
   FRAME_GUARD;
-  return ImGui::InvisibleButton(str_id, ImVec2(size_w, size_h), API_RO_GET(flags));
+  return ImGui::InvisibleButton(str_id, ImVec2(size_w, size_h), API_GET(flags));
 }
 
 API_FUNC(0_1, bool, ArrowButton, (Context*,ctx)
-(const char*,str_id)(int,dir),
+(const char*,str_id) (int,dir),
 "Square button with an arrow shape. 'dir' is one of the Dir_* values")
 {
   FRAME_GUARD;
@@ -58,25 +58,25 @@ API_FUNC(0_1, bool, ArrowButton, (Context*,ctx)
 }
 
 API_FUNC(0_1, bool, Checkbox, (Context*,ctx)
-(const char*,label)(bool*,API_RW(v)),
+(const char*,label) (RW<bool*>,v),
 "")
 {
   FRAME_GUARD;
-  if(!API_RW(v))
+  if(!v)
     return false;
-  return ImGui::Checkbox(label, API_RW(v));
+  return ImGui::Checkbox(label, v);
 }
 
 API_FUNC(0_1, bool, CheckboxFlags, (Context*,ctx)
-(const char*,label)(int*,API_RW(flags))(int,flags_value),
+(const char*,label) (RW<int*>,flags) (int,flags_value),
 "")
 {
   FRAME_GUARD;
-  return ImGui::CheckboxFlags(label, API_RW(flags), flags_value);
+  return ImGui::CheckboxFlags(label, flags, flags_value);
 }
 
 API_FUNC(0_1, bool, RadioButton, (Context*,ctx)
-(const char*,label)(bool,active),
+(const char*,label) (bool,active),
 R"(Use with e.g. if (RadioButton("one", my_value==1)) { my_value = 1; })")
 {
   FRAME_GUARD;
@@ -84,11 +84,11 @@ R"(Use with e.g. if (RadioButton("one", my_value==1)) { my_value = 1; })")
 }
 
 API_FUNC(0_1, bool, RadioButtonEx, (Context*,ctx)
-(const char*,label)(int*,API_RW(v))(int,v_button),
+(const char*,label) (RW<int*>,v) (int,v_button),
 "Shortcut to handle RadioButton's example pattern when value is an integer")
 {
   FRAME_GUARD;
-  return ImGui::RadioButton(label, API_RW(v), v_button);
+  return ImGui::RadioButton(label, v, v_button);
 }
 
 API_FUNC(0_1, void, PushButtonRepeat, (Context*,ctx)

api/context.cpp
@@ -22,14 +22,14 @@
 API_SECTION("Context");
 
 API_FUNC(0_5, Context*, CreateContext,
-(const char*,label)(int*,API_RO(config_flags),ImGuiConfigFlags_None),
+(const char*,label) (RO<int*>,config_flags,ImGuiConfigFlags_None),
 R"(Create a new ReaImGui context.
 The context will remain valid as long as it is used in each defer cycle.
 
 The label is used for the tab text when windows are docked in REAPER
 and also as a unique identifier for storing settings.)")
 {
-  return new Context { label, API_RO_GET(config_flags) };
+  return new Context { label, API_GET(config_flags) };
 }
 
 API_FUNC(0_1, double, GetTime, (Context*,ctx),
@@ -61,7 +61,7 @@ GetDeltaTime), in frame per second. Solely for convenience.)")
   return ctx->IO().Framerate;
 }
 
-API_FUNC(0_8, void, Attach, (Context*,ctx)(Resource*,obj),
+API_FUNC(0_8, void, Attach, (Context*,ctx) (Resource*,obj),
 R"(Link the object's lifetime to the given context.
 Objects can be draw list splitters, fonts, images, list clippers, etc.
 Call Detach to let the object be garbage-collected after unuse again.
@@ -78,7 +78,7 @@ context per defer cycle. See "limitations" in the font API documentation.)")
   ctx->attach(obj);
 }
 
-API_FUNC(0_8, void, Detach, (Context*,ctx)(Resource*,obj),
+API_FUNC(0_8, void, Detach, (Context*,ctx) (Resource*,obj),
 R"(Unlink the object's lifetime. Unattached objects are automatically destroyed
 when left unused. You may check whether an object has been destroyed using
 ValidatePtr.)")
@@ -136,7 +136,7 @@ static constexpr IOFields<bool, float, int> g_configVars[] {
 };
 
 #define API_CONFIGVAR(vernum, name, doc) \
-  API_FUNC(vernum, int, ConfigVar##_##name, NO_ARGS, doc) \
+  API_FUNC(vernum, int, ConfigVar##_##name, API_NO_ARGS, doc) \
     { return __COUNTER__ - baseConfigVar - 1; }
 
 constexpr int baseConfigVar { __COUNTER__ };
@@ -250,7 +250,7 @@ API_FUNC(0_7, double, GetConfigVar, (Context*,ctx)
 }
 
 API_FUNC(0_7, void, SetConfigVar, (Context*,ctx)
-(int,var_idx)(double,value),
+(int,var_idx) (double,value),
 "")
 {
   assertValid(ctx);

api/dragndrop.cpp
@@ -39,7 +39,7 @@ static bool isUserType(const char *type)
 }
 
 API_FUNC(0_1, bool, BeginDragDropSource, (Context*,ctx)
-(int*,API_RO(flags),ImGuiDragDropFlags_None),
+(RO<int*>,flags,ImGuiDragDropFlags_None),
 R"(Call after submitting an item which may be dragged. when this return true,
 you can call SetDragDropPayload() + EndDragDropSource()
 
@@ -48,11 +48,11 @@ it won't have a preview tooltip (we currently display a fallback "..." tooltip
 as replacement).)")
 {
   FRAME_GUARD;
-  return ImGui::BeginDragDropSource(API_RO_GET(flags));
+  return ImGui::BeginDragDropSource(API_GET(flags));
 }
 
 API_FUNC(0_1, bool, SetDragDropPayload, (Context*,ctx)
-(const char*,type)(const char*,data)(int*,API_RO(cond),ImGuiCond_Always),
+(const char*,type) (const char*,data) (RO<int*>,cond,ImGuiCond_Always),
 R"(The type is a user defined string of maximum 32 characters.
 Strings starting with '_' are reserved for dear imgui internal types.
 Data is copied and held by imgui.)")
@@ -70,7 +70,7 @@ Data is copied and held by imgui.)")
       data = nullptr; // imgui asserts if data != null && size == 0
   }
 
-  return ImGui::SetDragDropPayload(type, data, dataSize, API_RO_GET(cond));
+  return ImGui::SetDragDropPayload(type, data, dataSize, API_GET(cond));
 }
 
 API_FUNC(0_1, void, EndDragDropSource, (Context*,ctx),
@@ -90,8 +90,8 @@ If this returns true, you can call AcceptDragDropPayload + EndDragDropTarget.)")
 
 API_FUNC(0_1, bool, AcceptDragDropPayload, (Context*,ctx)
 (const char*,type)
-(char*,API_WBIG(payload))(int,API_WBIG_SZ(payload))
-(int*,API_RO(flags),ImGuiDragDropFlags_None),
+(WB<char*>,payload) (WBS<int>,payload_sz)
+(RO<int*>,flags,ImGuiDragDropFlags_None),
 R"(Accept contents of a given type. If DragDropFlags_AcceptBeforeDelivery is set
 you can peek into the payload before the mouse button is released.)")
 {
@@ -100,15 +100,12 @@ you can peek into the payload before the mouse button is released.)")
   if(!isUserType(type))
     return false;
 
-  const ImGuiDragDropFlags flags { API_RO_GET(flags) };
-  const ImGuiPayload *payload { ImGui::AcceptDragDropPayload(type, flags) };
-  if(!payload)
+  const ImGuiPayload *impayload { ImGui::AcceptDragDropPayload(type, API_GET(flags)) };
+  if(!impayload)
     return false;
 
-  if(API_WBIG(payload) && payload->Data) {
-    copyToBigBuf(API_WBIG(payload), API_WBIG_SZ(payload),
-      payload->Data, payload->DataSize);
-  }
+  if(payload && impayload->Data)
+    copyToBigBuf(payload, payload_sz, impayload->Data, impayload->DataSize);
 
   return true;
 }
@@ -135,34 +132,34 @@ static bool AcceptDragDropPayloadColor(int *color, bool alpha,
 }
 
 API_FUNC(0_1, bool, AcceptDragDropPayloadRGB, (Context*,ctx)
-(int*,API_W(rgb))(int*,API_RO(flags),ImGuiDragDropFlags_None),
+(W<int*>,rgb) (RO<int*>,flags,ImGuiDragDropFlags_None),
 "Accept a RGB color. See AcceptDragDropPayload.")
 {
   FRAME_GUARD;
-  return AcceptDragDropPayloadColor(API_W(rgb), false, API_RO_GET(flags));
+  return AcceptDragDropPayloadColor(rgb, false, API_GET(flags));
 }
 
 API_FUNC(0_1, bool, AcceptDragDropPayloadRGBA, (Context*,ctx)
-(int*,API_W(rgba))(int*,API_RO(flags),ImGuiDragDropFlags_None),
+(W<int*>,rgba) (RO<int*>,flags,ImGuiDragDropFlags_None),
 "Accept a RGBA color. See AcceptDragDropPayload.")
 {
   FRAME_GUARD;
-  return AcceptDragDropPayloadColor(API_W(rgba), true, API_RO_GET(flags));
+  return AcceptDragDropPayloadColor(rgba, true, API_GET(flags));
 }
 
 API_FUNC(0_1, bool, AcceptDragDropPayloadFiles, (Context*,ctx)
-(int*,API_W(count))(int*,API_RO(flags),ImGuiDragDropFlags_None),
+(W<int*>,count) (RO<int*>,flags,ImGuiDragDropFlags_None),
 R"(Accept a list of dropped files. See AcceptDragDropPayload and GetDragDropPayloadFile.)")
 {
   FRAME_GUARD;
-  assertValid(API_W(count));
+  assertValid(count);
 
   const ImGuiPayload *payload {
-    ImGui::AcceptDragDropPayload(REAIMGUI_PAYLOAD_TYPE_FILES, API_RO_GET(flags))
+    ImGui::AcceptDragDropPayload(REAIMGUI_PAYLOAD_TYPE_FILES, API_GET(flags))
   };
 
   if(payload)
-    *API_W(count) = ctx->draggedFiles().size();
+    *count = ctx->draggedFiles().size();
 
   return payload;
 }
@@ -175,32 +172,30 @@ API_FUNC(0_1, void, EndDragDropTarget, (Context*,ctx),
 }
 
 API_FUNC(0_1, bool, GetDragDropPayload, (Context*,ctx)
-(char*,API_W(type))(int,API_W_SZ(type))
-(char*,API_WBIG(payload))(int,API_WBIG_SZ(payload))
-(bool*,API_W(is_preview))(bool*,API_W(is_delivery)),
+(W<char*>,type) (WS<int>,type_sz)
+(WB<char*>,payload) (WBS<int>,payload_sz)
+(W<bool*>,is_preview) (W<bool*>,is_delivery),
 R"(Peek directly into the current payload from anywhere.
 Returns false when drag and drop is finished or inactive.)")
 {
   FRAME_GUARD;
 
-  const ImGuiPayload *payload { ImGui::GetDragDropPayload() };
-  if(!payload || payload->DataFrameCount == -1 || !isUserType(payload->DataType))
+  const ImGuiPayload *impayload { ImGui::GetDragDropPayload() };
+  if(!impayload || impayload->DataFrameCount == -1 || !isUserType(impayload->DataType))
     return false;
 
-  if(API_W(type))
-    snprintf(API_W(type), API_W_SZ(type), "%s", payload->DataType);
-  if(API_WBIG(payload) && payload->Data) {
-    copyToBigBuf(API_WBIG(payload), API_WBIG_SZ(payload),
-      payload->Data, payload->DataSize);
-  }
-  if(API_W(is_preview))  *API_W(is_preview)  = payload->Preview;
-  if(API_W(is_delivery)) *API_W(is_delivery) = payload->Delivery;
+  if(type)
+    snprintf(type, type_sz, "%s", impayload->DataType);
+  if(payload && impayload->Data)
+    copyToBigBuf(payload, payload_sz, impayload->Data, impayload->DataSize);
+  if(is_preview)  *is_preview  = impayload->Preview;
+  if(is_delivery) *is_delivery = impayload->Delivery;
 
   return true;
 }
 
 API_FUNC(0_1, bool, GetDragDropPayloadFile, (Context*,ctx)
-(int,index)(char*,API_W(filename))(int,API_W_SZ(filename)),
+(int,index) (W<char*>,filename) (WS<int>,filename_sz),
 R"(Get a filename from the list of dropped files.
 Returns false if index is out of bounds.)")
 {
@@ -214,7 +209,7 @@ Returns false if index is out of bounds.)")
   else if(static_cast<size_t>(index) >= files.size())
     return false;
 
-  snprintf(API_W(filename), API_W_SZ(filename), "%s", files[index].c_str());
+  snprintf(filename, filename_sz, "%s", files[index].c_str());
 
   return true;
 }

api/font.cpp
@@ -35,7 +35,7 @@ This API currently has multiple limitations (v1.0 blockers):
   of the defer loop.))");
 
 API_FUNC(0_9, Font*, CreateFont,
-(const char*,family_or_file)(int,size)(int*,API_RO(flags),ReaImGuiFontFlags_None),
+(const char*,family_or_file) (int,size) (RO<int*>,flags,ReaImGuiFontFlags_None),
 R"(Load a font matching a font family name or from a font file.
 The font will remain valid while it's attached to a context. See Attach.
 
@@ -46,7 +46,7 @@ If 'family_or_file' specifies a path to a font file (contains a / or \\):
 - The first byte of 'flags' is used as the font index within the file
 - The font styles in 'flags' are simulated by the font renderer)")
 {
-  return new Font { family_or_file, size, API_RO_GET(flags) };
+  return new Font { family_or_file, size, API_GET(flags) };
 }
 
 API_FUNC(0_4, Font*, GetFont, (Context*,ctx),

api/function.cpp
@@ -28,7 +28,8 @@ such as InputText* and SetNextWindowSizeConstraints.
 They can also be used standalone with Function_Execute
 (eg. faster-than-Lua DSP processing).)");
 
-API_FUNC(0_9, Function*, CreateFunctionFromEEL, (const char*,code),
+API_FUNC(0_9, Function*, CreateFunctionFromEEL,
+(const char*,code),
 R"(Compile an EEL program.
 
 Standard EEL [math](https://www.reaper.fm/sdk/js/basiccode.php#js_basicfunc)
@@ -46,7 +47,8 @@ API_FUNC(0_8_5, void, Function_Execute, (Function*,func),
   func->execute();
 }
 
-API_FUNC(0_8_5, double, Function_GetValue, (Function*,func)(const char*,name),
+API_FUNC(0_8_5, double, Function_GetValue, (Function*,func)
+(const char*,name),
 "")
 {
   assertValid(func);
@@ -56,7 +58,7 @@ API_FUNC(0_8_5, double, Function_GetValue, (Function*,func)(const char*,name),
 }
 
 API_FUNC(0_8_5, void, Function_SetValue, (Function*,func)
-(const char*,name)(double,value),
+(const char*,name) (double,value),
 "")
 {
   assertValid(func);
@@ -65,7 +67,7 @@ API_FUNC(0_8_5, void, Function_SetValue, (Function*,func)
 }
 
 API_FUNC(0_8_5, void, Function_GetValue_Array, (Function*,func)
-(const char*,name)(reaper_array*,values),
+(const char*,name) (reaper_array*,values),
 R"(Copy the values in the function's memory starting at the address stored
 in the given variable into the array.)")
 {
@@ -76,7 +78,7 @@ in the given variable into the array.)")
 }
 
 API_FUNC(0_8_5, void, Function_SetValue_Array, (Function*,func)
-(const char*,name)(reaper_array*,values),
+(const char*,name) (reaper_array*,values),
 R"(Copy the values in the array to the function's memory at the address stored
 in the given variable.)")
 {
@@ -87,20 +89,20 @@ in the given variable.)")
 }
 
 API_FUNC(0_8_5, void, Function_GetValue_String, (Function*,func)
-(const char*,name)(char*,API_WBIG(value))(int,API_WBIG_SZ(value)),
+(const char*,name) (WB<char*>,value) (WBS<int>,value_sz),
 "Read from a string slot or a named string (when name starts with a `#`).")
 {
   assertValid(func);
-  assertValid(API_WBIG(value));
+  assertValid(value);
 
-  const auto &value { func->getString(name) };
-  if(!value)
+  const auto &string { func->getString(name) };
+  if(!string)
     throw reascript_error { "could not read string value" };
-  copyToBigBuf(API_WBIG(value), API_WBIG_SZ(value), *value);
+  copyToBigBuf(value, value_sz, *string);
 }
 
 API_FUNC(0_8_5, void, Function_SetValue_String, (Function*,func)
-(const char*,name)(const char*,value)(int,value_sz),
+(const char*,name) (const char*,value) (int,value_sz),
 "Write to a string slot or a named string (when name starts with a `#`).")
 {
   assertValid(func);

api/helper.hpp
@@ -58,16 +58,20 @@ DECLARE_HANDLE(HDROP);
 #define _API_STRARR_US(r, macro, i, arg) \
   BOOST_PP_EXPR_IF(i, "\31") BOOST_PP_STRINGIZE(macro(arg))
 
-template<typename T>
-using DefArgVal = std::conditional_t<
-  std::is_same_v<const char *, T>, T, std::remove_pointer_t<T>
->;
+template<typename T, typename = void>
+struct DefVal { using type = std::remove_pointer_t<T>; };
+
+template<>
+struct DefVal<const char *> { using type = const char *; };
+
+template<typename T, auto tags>
+struct DefVal<Tag<T, tags>> { using type = typename DefVal<T>::type; };
 
 #define _API_DEFARG_ID(argName) BOOST_PP_CAT(argName, Default)
 #define _API_DEFARG(r, name, i, arg)                          \
   BOOST_PP_EXPR_IF(                                           \
     BOOST_PP_GREATER_EQUAL(BOOST_PP_TUPLE_SIZE(arg), 3),      \
-    constexpr DefArgVal<_API_ARG_TYPE(arg)>                   \
+    constexpr DefVal<_API_ARG_TYPE(arg)>::type                \
       _API_ARG_NAME(arg)(_API_ARG_DEFV(arg)); \
   )
 
@@ -112,8 +116,6 @@ using DefArgVal = std::conditional_t<
 #define _API_DEF(vernum, name, named) \
   CompStr::apidef<&API::v##vernum::name::impl, API::v##vernum::name::meta, named>
 
-#define API_DO_NOT_USE "Internal use only."
-
 #define API_FUNC _API_STORE_LINE _API_FUNC
 #define _API_FUNC(vernum, type, name, args, help)                   \
   _API_CHECKROOTSECTION                                             \
@@ -130,7 +132,7 @@ using DefArgVal = std::conditional_t<
 
 #define API_ENUM _API_STORE_LINE _API_ENUM
 #define _API_ENUM(vernum, prefix, name, doc) \
-  _API_FUNC(vernum, int, name, NO_ARGS, doc) { return prefix##name; }
+  _API_FUNC(vernum, int, name, API_NO_ARGS, doc) { return prefix##name; }
 
 #define API_EELFUNC _API_STORE_LINE _API_EELFUNC
 #define _API_EELFUNC(vernum, type, name, args, help)    \
@@ -169,29 +171,15 @@ using DefArgVal = std::conditional_t<
 #define API_SECTION_P(parent, ...) \
   API_SECTION_DEF(_API_UNIQ_SEC_ID, parent,       __VA_ARGS__)
 
-// TODO: replace these in favor of the new type tags from types.hpp
-#define NO_ARGS (,)
-#define API_RO(var)       var##InOptional // read, optional/nullable (except string, use nullIfEmpty)
-#define API_RW(var)       var##InOut      // read/write
-#define API_RWO(var)      var##InOutOptional // documentation/python only
-#define API_W(var)        var##Out        // write
-#define API_W_SZ(var)     var##Out_sz     // write
-// Not using varInOutOptional because REAPER refuses to pass NULL to them
-#define API_RWBIG(var)    var##InOutNeedBig    // read/write, resizable (realloc_cmd_ptr)
-#define API_RWBIG_SZ(var) var##InOutNeedBig_sz // size of previous API_RWBIG buffer
-#define API_WBIG(var)     var##OutNeedBig
-#define API_WBIG_SZ(var)  var##OutNeedBig_sz
+#define API_DO_NOT_USE "Internal use only."
+#define API_NO_ARGS (,)
 
-#define _API_GET(var) [](const auto v, const auto d) { \
-  if constexpr(std::is_pointer_v<decltype(d)>)         \
-    return v ?  v : d;                                 \
-  else                                                 \
-    return v ? *v : d;                                 \
+#define API_GET(var) [](const auto &v, const auto &d) -> const auto & { \
+  if constexpr(std::is_pointer_v<std::remove_reference_t<decltype(d)>>) \
+    return v ? static_cast<decltype(d)>(v) : d; \
+  else                                          \
+    return v ? *v : d;                          \
 }(var, defaults::var)
-#define API_RO_GET(var)  _API_GET(API_RO(var))
-#define API_RWO_GET(var) _API_GET(API_RWO(var))
-
-#define FRAME_GUARD assertValid(ctx); assertFrame(ctx)
 
 // const char *foobarInOptional from REAPER are never null before 6.58
 inline void nullIfEmpty(const char *&string)
@@ -217,6 +205,12 @@ void assertValid(T *ptr)
   Error::invalidObject(ptr);
 }
 
+template<typename T, auto tags>
+void assertValid(Tag<T, tags> ptr)
+{
+  return assertValid(static_cast<T>(ptr));
+}
+
 inline void assertFrame(Context *ctx)
 {
   if(!ctx->enterFrame()) {
@@ -224,6 +218,8 @@ inline void assertFrame(Context *ctx)
     throw reascript_error { "frame initialization failed" };
   }
 }
+
+#define FRAME_GUARD assertValid(ctx); assertFrame(ctx)
 
 template <typename PtrType, typename ValType, size_t N>
 class ReadWriteArray {

commit 6e613ee407fc36d195819a93f79313f88c43ad03

fix omitting ColorPick4's ref_col parameter
(regression from 519a4b9d655e1dd041709c8f8adf1a25c4b94f35)

api/coloredit.cpp
@@ -80,7 +80,7 @@ API_FUNC(0_1, bool, ColorPicker4, (Context*,ctx)
   if(ref_col)
     Color(*ref_col, alpha).unpack(refCol);
 
-  if(ImGui::ColorPicker4(label, col, clean_flags, refCol)) {
+  if(ImGui::ColorPicker4(label, col, clean_flags, ref_col ? refCol : nullptr)) {
     // preserves unused bits from the input integer as-is (eg. REAPER's enable flag)
     *col_rgba = Color{col}.pack(alpha, *col_rgba);
     return true;