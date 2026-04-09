cfillion/reaimgui

commit 4239611e2ed798cd2ec2b5a80ea69cadb2178e9d

win32: extend hit-test transparency to windows in other processes
WS_EX_LAYERED may not be supported on Windows earlier than 8?

src/win32_window.cpp
@@ -85,7 +85,7 @@ static bool useBigFrame()
 void Win32Window::updateStyles()
 {
   m_style = WS_POPUP; // fix AttachWindowTopmostButton when a titlebar is shown
-  m_exStyle = WS_EX_ACCEPTFILES;
+  m_exStyle = WS_EX_ACCEPTFILES | WS_EX_LAYERED;
 
   if(!(m_viewport->Flags & ImGuiViewportFlags_NoDecoration)) {
     m_style |= WS_OVERLAPPEDWINDOW & ~WS_MINIMIZEBOX;
@@ -99,6 +99,9 @@ void Win32Window::updateStyles()
 
   if(m_viewport->Flags & ImGuiViewportFlags_TopMost)
     m_exStyle |= WS_EX_TOPMOST;
+
+  if(m_viewport->Flags & ImGuiViewportFlags_NoInputs)
+    m_exStyle |= WS_EX_TRANSPARENT;
 }
 
 static BOOL CALLBACK reparentChildren(HWND hwnd, LPARAM data)
@@ -164,6 +167,7 @@ void Win32Window::create()
   const RECT &rect {scaledWindowRect(m_viewport->Pos, m_viewport->Size)};
   SetWindowPos(m_hwnd, nullptr,  rect.left, rect.top,
     rect.right - rect.left, rect.bottom - rect.top, SWP_NOACTIVATE | SWP_NOZORDER);
+  SetLayeredWindowAttributes(m_hwnd, 0, 0xFF, LWA_ALPHA);
 
   try {
     m_renderer = m_ctx->rendererFactory()->create(this);
@@ -270,16 +274,7 @@ void Win32Window::setTitle(const char *title)
 
 void Win32Window::setAlpha(const float alpha)
 {
-  // may conflict with updateStyles()/update(), but viewport flags are
-  // unlikely to change while alpha isn't 1
-
-  if(alpha == 1.0f) {
-    SetWindowLong(m_hwnd, GWL_EXSTYLE, m_exStyle);
-    return;
-  }
-
-  SetWindowLong(m_hwnd, GWL_EXSTYLE, m_exStyle | WS_EX_LAYERED);
-  SetLayeredWindowAttributes(m_hwnd, 0, 255 * alpha, LWA_ALPHA);
+  SetLayeredWindowAttributes(m_hwnd, 0, 0xFF * alpha, LWA_ALPHA);
 }
 
 void Win32Window::update()

commit 545f1802a29c9554c033120f729848301c40d650

win32: fix docked windows being invisible until resized [p=2934941]
Regression from 4239611e2ed798cd2ec2b5a80ea69cadb2178e9d

The original fix was required for WindowFlags_NoInputs topmost windows to work
properly over other processes' windows (eg. bridged plugin).

src/win32_window.cpp
@@ -85,7 +85,9 @@ static bool useBigFrame()
 void Win32Window::updateStyles()
 {
   m_style = WS_POPUP; // fix AttachWindowTopmostButton when a titlebar is shown
-  m_exStyle = WS_EX_ACCEPTFILES | WS_EX_LAYERED;
+  m_exStyle = WS_EX_ACCEPTFILES;
+  if(!isDocked())
+    m_exStyle |= WS_EX_LAYERED;
 
   if(!(m_viewport->Flags & ImGuiViewportFlags_NoDecoration)) {
     m_style |= WS_OVERLAPPEDWINDOW & ~WS_MINIMIZEBOX;