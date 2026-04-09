cfillion/reaimgui

commit cd254c03d221e9735cc8358726a31c915637b9d5

gdk: trick Hyprland into removing its borders
https://github.com/hyprwm/Hyprland/blob/da04afa44e7545771b08f8e0defd2cd1810f1613/src/managers/XWaylandManager.cpp#L144

src/gdk_window.cpp
@@ -110,6 +110,9 @@ void GDKWindow::show()
   Window::show();
   initIME();
   m_renderer = m_ctx->rendererFactory()->create(this);
+
+  // makes Hyprland disable the borders
+  gdk_window_set_type_hint(getOSWindow(), GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
 }
 
 void GDKWindow::setPosition(ImVec2 pos)

commit 02cbc1d94e5aab1212ad7a73d0386e114e922226

fix broken docking on Linux
Regression from cd254c03d221e9735cc8358726a31c915637b9d5

src/gdk_window.cpp
@@ -112,7 +112,8 @@ void GDKWindow::show()
   m_renderer = m_ctx->rendererFactory()->create(this);
 
   // makes Hyprland disable the borders
-  gdk_window_set_type_hint(getOSWindow(), GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
+  if(!isDocked())
+    gdk_window_set_type_hint(getOSWindow(), GDK_WINDOW_TYPE_HINT_SPLASHSCREEN);
 }
 
 void GDKWindow::setPosition(ImVec2 pos)